5fbacdb2e6fc6cf56b46d016f68be16783c906f7
JTPond <jpond@jlab.org>
2014-12-12 14:34:41 -0500

    More sim stuff

--- isobarModel/farmSimulation/devTestFarm.py
+++ isobarModel/farmSimulation/devTestFarm.py
@@ -17,6 +17,7 @@ from pythonPWA.model.normInt import normInt
 from pythonPWA.model.intensity import intensity
 from pythonPWA.fileHandlers.gampReader import gampReader 
 from batchFarmServices.dataSimulatorNPY import dataSimulator
+from pythonPWA.model.complexV import complexV
 import operator
 
 from batchFarmServices.rhoAA import rhoAA
@@ -32,20 +33,40 @@ maxNumberOfEvents=float(len(alphaList))
 
 testMass= int(sys.argv[1])+(int(Control[4])/2.)
 
-resonances=[resonance(cR=2.*maxNumberOfEvents/3.,wR=[0.,1.],w0=1320.,r0=107.),resonance(cR=maxNumberOfEvents/3.,wR=[1.,0.],w0=1670.,r0=259.)]
-
-contents=numpy.load(os.path.join(dataDir,"Vvalues.npy"))    
-orderedContents=sorted(contents.tolist().iteritems(),key=operator.itemgetter(0))    
 productionAmplitudes=[]
-for i in range(0,len(orderedContents),2):
-    realPart=orderedContents[i][1]
-    imaginaryPart=orderedContents[i+1][1]
-    productionAmplitudes.append(numpy.complex(realPart,imaginaryPart))
-    
+
 waves=getwaves(os.path.join(dataDir,"mc","raw"))
-               
+
 normint=numpy.load(os.path.join(dataDir,"mc","raw","normint.npy"))
 
+if not os.path.isfile(os.path.join(dataDir,"Vvalues.npy")):
+    contents=numpy.load(os.path.join(dataDir,"Vvalues.npy"))    
+    orderedContents=sorted(contents.tolist().iteritems(),key=operator.itemgetter(0))     
+    for i in range(0,len(orderedContents),2):
+        realPart=orderedContents[i][1]
+        imaginaryPart=orderedContents[i+1][1]
+        productionAmplitudes.append(numpy.complex(realPart,imaginaryPart))  
+          
+elif os.path.isfile(os.path.join(topDir,"scripts","resonances.txt")):
+    resonances=[]
+    res = open("resonances.txt")
+    rez = res.readlines()
+    for re in rez:
+        if re[0] != "#" and re[0] != " " and re[0] != "\n":
+            rev = re.split(" ")
+            wRx = [(float(x)) for x in rev[1].split(",")]
+            resonances.append(resonance(cR=float(rev[0]),wR=wRx,w0=float(rev[2]),r0=float(rev[3])))        
+    for resonance in resonances:
+        print resonance.toString()                
+        for wave in waves:
+            productionAmplitudes.append(complexV(resonance,wave,waves,normint,testMass))
+    if len(productionAmplitudes) == 0:
+        print "There are no resonances in resonances.txt, modify it in /scripts and try again."
+        exit()
+else:
+    print "There is neither a resonance.txt file, or a Vvalues.npy file consult the documentation and try again."
+    exit()
+    
 if sys.argv[3] == "i":
     if os.path.isfile(os.path.join(dataDir,"mc","raw","rhoAA.npy")):
         rhoAA = numpy.load(os.path.join(dataDir,"mc","raw","rhoAA.npy"))
@@ -54,24 +75,24 @@ if sys.argv[3] == "i":
         rhoAA = rAA.calc()  
         numpy.save(os.path.join(dataDir,"mc","raw","rhoAA.npy"),rhoAA)
 
-    dSimulator=dataSimulator(mass=testMass,waves=waves,resonances=resonances,productionAmplitudes=productionAmplitudes,normint=normint,alphaList=alphaList,rhoAA=rhoAA)
+    dSimulator=dataSimulator(mass=testMass,waves=waves,productionAmplitudes=productionAmplitudes,normint=normint,alphaList=alphaList,rhoAA=rhoAA)
     iList = dSimulator.calcIList()
     numpy.save(os.path.join(dataDir,"mc","raw","iList"),iList)
 
-elif if sys.argv[3] == "s":
+elif sys.argv[3] == "s":
     inputGampFile=open(os.path.join(dataDir,"mc","raw","events.gamp"),'r')
     inputPfFile=open(os.path.join(dataDir,"mc","raw","events.pf"),'r')
     outputRawGampFile=open(os.path.join(dataDir,"data","selected_events.raw.gamp"),'w')
     outputAccGampFile=open(os.path.join(dataDir,"data","selected_events.acc.gamp"),'w')
-    
-    rhoAA = numpy.load(os.path.join(dataDir,"mc","raw","rhoAA.npy"))
-                
+                    
     iList = numpy.load(os.path.join(dataDir,"mc","raw","iList.npy"))
 
     iMax = numpy.load(os.path.join(dataDir,"mc","raw","iMax.npy"))
 
-    dSimulator=dataSimulator(mass=testMass,waves=waves,resonances=resonances,productionAmplitudes=productionAmplitudes,normint=normint,alphaList=alphaList,rhoAA=rhoAA,iList=iList,iMax=iMax)
+    dSimulator=dataSimulator(mass=testMass,waves=waves,productionAmplitudes=productionAmplitudes,normint=normint,alphaList=alphaList,rhoAA=rhoAA,iList=iList,iMax=iMax)
 
     dSimulator.execute(inputGampFile,outputRawGampFile,outputAccGampFile,inputPfFile)
                 
-
+else:
+    print "The last argument must be either i to calculate iList, or s to do the simulation."
+    exit()
