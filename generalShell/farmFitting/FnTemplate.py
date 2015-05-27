c3cbabaa240ddd18b92db77774aeeb5251805f44
JTPond <jpond@jlab.org>
2015-05-27 15:43:02 -0400

    nTrue and bugs

--- generalShell/farmFitting/FnTemplate.py
+++ generalShell/farmFitting/FnTemplate.py
@@ -1,32 +1,17 @@
 import numpy,sys, os
-from FourVec import FourVector
 from iminuit import Minuit
 
 def intFn(kVars,params): # Do not change the name of this function
     """
         This is where you define your intensity function. Do not change the name of the function. 
         The names of the arguments are up to you, but they both need to be dictionaries, with the 
-        first one being the kinematic variables, either from a gamp event or a list. And the second
+        first one being the kinematic variables from a list. And the second
         being the fitted parameters. All fitted parameters need to be floating point numbers. If a
         parameter of your function is a complex number make the real part one fitted variable and
         the imaginary part another. Your function should return a numpy.complex(real,imaginary) 
         as below. 
     """
     return numpy.complex((kVars['s']**2)*(kVars['t']**3)*params["A1"],params['A2']*kVars['u']) #example
-
-def ampFn(kVars,params):
-    """
-        This is the function that is called by generalShell. As you can see if you have a 
-        function that returns the intensity in intFn then this function will be the same as the 
-        default here, so you can ignore this function. If you don NOT have an intensity function, 
-        but instead an amplitude function, then comment out the return statement of intFn and add
-        the 'pass' keyword in its place. Then replace the contents of this function with your own 
-        code. 
-    """
-    
-    Int = intFn(kVars,params)
-    return Int*numpy.conjugate(Int)
-
 
 def parFn(A1,A2): # Do not change the name of this function
     """
@@ -43,17 +28,28 @@ def migFn(): # Do not change this line
     m = Minuit(parFn,**kwdarg) # or this one
     m.set_strategy(0) # set strategy as an int. 0: Fast 1: More accurate 2: Slowest/most accurate
     m.set_up(0.5) # Leave this line alone
-    m.migrad(ncall=1000) # set ncall >1000
+    m.migrad(ncall=1000) # set ncall >=1000
 
     Vvalues = m.values # Do not change
     numpy.save(os.path.join(".","Vvalues.npy"),Vvalues) # The first argument of numpy.save is the file path/name you  
                                                         # want to save the fitted values to
+def nTrueFn():
+    from calcNTrue import calcNTrue
+    genDir = "./kvArgsGen.txt"  #<------- This should be the only thing you have to set for the calculation of nTrue, just set it to the generated MC file. 
+    params = numpy.load("Vvalues.npy")
+    nT = calcNTrue(genDir)
+    nTrue = nT.calcNTrue(params)
 
 from generalShell import generalFit # Do not change this line
-gF = generalFit(dataDir="./kvArgs.txt",genDir="./kvArgsGen.txt",QDir="./QFactor.txt",initial={'A1':.01,'A2':.01})
+dataDir="./kvArgs.txt"
+accDir="./kvArgsAcc.txt"
+QDir="./QFactor.txt"
+initial={'A1':.01,'A2':.01,'errordef':0.5}#do not change errordef 
+gF = generalFit(dataDir=dataDir,accDir=accDir,QDir=QDir,initial=initial)
 """
     In the above line do not change the names of any of the arguments but set the values to the directories
-    Of your data file(either gamp or parameter list), generated MC file, list of Q Factors(If you do not account
+    Of your data file, accepted MC file, list of Q Factors(If you do not account
     for Q then leave it as an empty string("") and Q will be set to 1.0 for all events and be inconsequential),
-    and a dictionary of all initial values for fitted parameters. 
+    and a dictionary of all initial values for fitted parameters, as well as any other iMinuit fitting aruguments. 
+    See iMinuit docs for more information. 
 """
--- generalShell/farmFitting/generalShell.py
+++ generalShell/farmFitting/generalShell.py
@@ -2,34 +2,31 @@
 import numpy
 import os, sys
 from iminuit import Minuit
-from gampTranslator import gampTranslator
 from kvParser import kvParser
-from Fn import ampFn
-from Fn import kvFn
+from Fn import intFn
 from Fn import parFn
 from Fn import migFn
 
 class generalFit (object):
 
-    def __init__(self,dataDir=None,genDir=None,QDir=None,initial={}):
+    def __init__(self,dataDir=None,accDir=None,QDir=None,initial={}):
         self.dataDir = dataDir
-        self.genDir = genDir
+        self.accDir = accDir
         self.QDir = QDir
         self.initial = initial
-        self.dataT = gampTranslator(self.dataDir)
         if ".txt" in self.dataDir:
             if not os.path.isfile(self.dataDir.rstrip(".txt")+".npy"):
                 self.dataKV = kvParser(self.dataDir)
                 numpy.save(self.dataDir.rstrip(".txt")+".npy",self.dataKV)
             else:
                 self.dataKV = numpy.load(self.dataDir.rstrip(".txt")+".npy")
-            if not os.path.isfile(self.genDir.rstrip(".txt")+".npy"):
-                self.genKV = kvParser(self.genDir)
-                numpy.save(self.genDir.rstrip(".txt")+".npy",self.genKV)
+            if not os.path.isfile(self.accDir.rstrip(".txt")+".npy"):
+                self.accKV = kvParser(self.genDir)
+                numpy.save(self.accDir.rstrip(".txt")+".npy",self.accKV)
             else:
-                self.genKV = numpy.load(self.genDir.rstrip(".txt")+".npy")
+                self.accKV = numpy.load(self.accDir.rstrip(".txt")+".npy")
             self.dataLen = len(self.dataKV)
-            self.genLen = len(self.genKV)
+            self.accLen = len(self.accKV)
         if os.path.isfile(self.QDir):
            self.QList = numpy.loadtxt(self.QDir)
         else:
@@ -38,27 +35,21 @@ class generalFit (object):
     def getdKVars(self,i):
         return self.dataKV[i]
 
-    def getgKVars(self,i):
-        return self.genKV[i]
-
-    def calcIList(self,**kwargs):      
-        iList = numpy.zeros(shape = (self.dataLen))        
-        for i in range(self.dataLen):
-            iList[i] = self.ampFn(self.getKVars(i))
-        return iList
+    def getaKVars(self,i):
+        return self.accKV[i]   
 
     def calcLnLike(self,params): 
         iList = numpy.zeros(shape = (self.dataLen),dtype = numpy.complex)        
         for i in range(self.dataLen):
-            iList[i] = ampFn(self.getdKVars(i),params)
+            iList[i] = intFn(self.getdKVars(i),params)
             sys.stdout.write("int "+str(i)+"\r")
             sys.stdout.flush()
-        gList = numpy.zeros(shape = (self.genLen),dtype = numpy.complex)
-        for i in range(self.genLen):
-            sys.stdout.write("generated "+str(i)+"\r")
+        aList = numpy.zeros(shape = (self.accLen),dtype = numpy.complex)
+        for i in range(self.accLen):
+            sys.stdout.write("accepted "+str(i)+"\r")
             sys.stdout.flush()
-            gList[i] = ampFn(self.getgKVars(i),params)
-        val = -((self.QList*numpy.log(iList)).sum(0)) + ((1.0/float(self.genLen)) * gList.sum(0))
+            aList[i] = intFn(self.getaKVars(i),params)
+        val = -((self.QList*numpy.log(iList)).sum(0)) + ((1.0/float(self.accLen)) * aList.sum(0))
         print val.real.item()
         return val.real.item()
 
