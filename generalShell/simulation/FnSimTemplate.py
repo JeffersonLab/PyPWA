b5ea3ae849cf7171ce06bee160c0a5ea6614436a
JTPond <jpond@jlab.org>
2015-06-23 14:04:28 -0400

    Sim rewrite

--- generalShell/simulation/FnSimTemplate.py
+++ generalShell/simulation/FnSimTemplate.py
@@ -14,49 +14,6 @@ def intFn(kVars,params): # Do not change the name of this function
         the imaginary part another. Your function should return a float. 
     """
     return (kVars['s']**2)*(kVars['t']**3)*params["A1"] #example
-
-def kvFn(event): # Do not change anything on this line    
-    """
-        This is where you define the function that accepts a pythonPWA gamp event object and returns
-        a keyed dictionary of the kinematic variables of the event for your intensity. This is an example 
-        of best practices for calculating the Mandulstrum variables from a gamma P -> Pi+ Pi- Pi0 event. 
-    """
-    
-    for particles in event.particles:
-        if particles.particleID == 14.0: #recoil proton
-            p = FourVector(float(particles.particleE), 
-                            float(particles.particleXMomentum),
-                            float(particles.particleYMomentum),
-                            float(particles.particleZMomentum))
-        if particles.particleID == 1.0: #photon gamma
-            bm = FourVector(float(particles.particleE), 
-                            float(particles.particleXMomentum),
-                            float(particles.particleYMomentum),
-                            float(particles.particleZMomentum))
-        if particles.particleID == 9.0: #p-
-            pim = FourVector(float(particles.particleE), 
-                            float(particles.particleXMomentum),
-                            float(particles.particleYMomentum),
-                            float(particles.particleZMomentum))
-        if particles.particleID == 8.0: #p+
-            pip = FourVector(float(particles.particleE), 
-                            float(particles.particleXMomentum),
-                            float(particles.particleYMomentum),
-                            float(particles.particleZMomentum))
-    ptarget = FourVector(.938, 0.,0.,0.)
-    initp = bm + ptarget
-    finalp = pip + pim + p
-    pi0 = initp - finalp
-    P1 = FourVector(bm.E,0.0,0.0,bm.E)
-    P2 = ptarget
-    P3 = pip + pim + pi0
-    P4 = p 
-
-    s= (P1+P2).dot(P1+P2)
-    t= (P1-P3).dot(P1-P3)
-    u= (P1-P4).dot(P1-P4)
-
-    return {'s':s,'t':t,'u':u} #example
  
 def simFn():
     """
@@ -64,23 +21,20 @@ def simFn():
     """
     
     nTrueDir=""#Location to save the nTrue File
-    inputGampDir=""#Loaction of the Generated MC Gamp File
-    inputPfDir=""#Location of the Generated acceptance PF file
-    outputPFGampDir=""#Location where the accepted, unweighted Gamp file will be saved
-    outputRawGampDir=""#Location where the unaccepted, weighted Gamp file will be saved
-    outputAccGampDir=""#Location where the accepted, weighted Gamp file will be saved
+    inputKVDir=""#Loaction of the Generated MC KV text File
+    outputWeightDir=""#Location where the weighted mask file will be saved
                     
     iList = numpy.load("")#Location of the list of intensities. 
 
-    iMax = numpy.load("")#Location of the Maximum intensity of the WHOLE fit
+    iMax = numpy.load("")#Location of the Maximum intensity of the WHOLE mass range
                 
-    gS.simulate(nTrueDir,inputGampDir,outputRawGampDir,outputAccGampDir,inputPfDir,outputPFGampDir,iList,iMax)
+    gS.simulate(nTrueDir,inputKVDir,outputWeightDir,iList,iMax)
 
 
 from generalSim import generalSim
-inputGampDir=""#Loaction of the Generated MC Gamp File(Same as above)
+inputKVDir=""#Loaction of the Generated MC KV File(Same as above)
 reLoad = False#Boolean value for reparsing the generated MC gamp file and overwriting its npy files
-gS = generalSim(gampDir = inputGampDir,reLoad=reLoad)
+gS = generalSim(KVDir = inputKVDir,reLoad=reLoad)
 if sys.argv[1] == "i":
     numpy.save("",gS.calcIList({'A1':7.,'A2':-3.0,'A3':0.37,'A4':0.037,'A5':0.121}))#example The first argument of numpy.save() is the filepath of the iList to be saved.
 elif sys.argv[1] == "s":
--- generalShell/simulation/generalSim.py
+++ generalShell/simulation/generalSim.py
@@ -1,49 +1,34 @@
 #! /u/apps/anaconda/bin/python2.7
 import numpy
 import os, sys
-from pythonPWA.fileHandlers.gampTranslator import gampTranslator
 from FnSim import intFn
-from FnSim import kvFn
 from random import random
 
 class generalSim (object):
 
-    def __init__(self,gampDir,reLoad):
-        self.gampDir = gampDir
-        self.gampT = gampTranslator(self.gampDir)
+    def __init__(self,KVDir,reLoad):
+        self.KVDir = KVDir
         self.reLoad = reLoad
-        if not os.path.isfile(self.gampDir.rstrip(".gamp")+".npy") or self.reLoad:
-            numpy.save(inputGampDir.rstrip(".gamp")+".npy",gampT.translate(inputGampDir.rstrip(".gamp")+".npy"))
-        self.gampT.events=numpy.load(self.gampDir.rstrip(".gamp")+".npy") 
-        self.dataLen = self.gampT.events.shape[0]
-
-    def getdKVars(self,i):
-        if ".gamp" in self.gampDir:
-            return kvFn(self.gampT.writeEvent(self.gampT.events[i,:,:]))   
+        if ".txt" in self.KVDir:
+            if not os.path.isfile(self.KVDir.rstrip(".txt")+".npy") or self.reLoad:
+                self.KVList = kvParser(self.KVDir)
+                numpy.save(self.KVDir.rstrip(".txt")+".npy",self.KVList)
+            else:
+                self.KVList = numpy.load(self.KVDir.rstrip(".txt")+".npy")
+        self.KVLen = len(self.KVList)
        
     def calcIList(self,params): 
-    	iList = numpy.zeros(shape = (self.dataLen))        
-        for i in range(self.dataLen):
-            iList[i] = intFn(self.getdKVars(i),params)
+    	iList = numpy.zeros(shape = (self.KVLen))        
+        for i in range(self.KVLen):
+            iList[i] = intFn(self.KVList[i],params)
             sys.stdout.write("int "+str(i)+"\r")
             sys.stdout.flush()
         return iList
 
-    def simulate(self,nTrueDir,inputGampDir,outputRawGampDir,outputAccGampDir,inputPfDir,outputPFGampDir,iList,iMax):
+    def simulate(self,nTrueDir,inputKVDir,outputWeightDir,iList,iMax):
         
-        outputPFGampFile=open(outputPFGampDir,'w')    
-        outputRawGampFile=open(outputRawGampDir,'w')
-        outputAccGampFile=open(outputAccGampDir,'w')
         nTrueList = [((1.0/(iList.shape[0]))*(iList.sum(0)))]  
         numpy.save(nTrueDir,nTrueList)
-
-        gampT = gampTranslator(inputGampDir)
-        if not os.path.isfile(inputGampDir.rstrip(".gamp")+".npy"):
-            numpy.save(inputGampDir.rstrip(".gamp")+".npy",gampT.translate(inputGampDir.rstrip(".gamp")+".npy"))
-        gampList=numpy.load(inputGampDir.rstrip(".gamp")+".npy") 
-      
-        
-        pflist = numpy.loadtxt(inputPfDir)
 
         wList=iList[:]/iMax
         
@@ -53,18 +38,8 @@ class generalSim (object):
             if wList[wn]>random():                
                 wnList[wn] = 1 
        	
-        numpy.save(os.path.join(os.path.split(inputGampDir)[0],"wMaskList"),wnList)
+        numpy.save(outputWeightDir,wnList)
 
-        for wn in range(len(wnList)):
-            wnEvent = gampT.writeEvent(gampList[wn,:,:])
-            if float(pflist[wn])==1.0:
-                wnEvent.writeGamp(outputPFGampFile)
-            if wnList[wn] == 1:                
-                wnEvent.writeGamp(outputRawGampFile)
-            if wnList[wn] == 1 and float(pflist[wn])==1.0:
-                wnEvent.writeGamp(outputAccGampFile)
-        outputPFGampFile.close()
-        outputRawGampFile.close()
-        outputAccGampFile.close()
+        
 
     
