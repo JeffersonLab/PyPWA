a82c273c634b48d6301e5b45e1681c1ffd1ae9f0
JTPond <jpond@jlab.org>
2015-07-09 15:11:53 -0400

    new stuff

--- pythonPWA/batchFarmServices/likelihoodTest.py
+++ pythonPWA/batchFarmServices/likelihoodTest.py
@@ -19,8 +19,20 @@ sys.path.append(os.path.join(sys.argv[2],"pythonPWA"))
 from pythonPWA.fileHandlers.getWavesGen import getwaves
 from batchFarmServices.fast_like import FASTLikelihood
 from batchFarmServices.rhoAA import rhoAA
-import fnGenerator
 from iminuit import Minuit
+
+def minFn(*args):
+    retList=[numpy.complex(args[i],args[i+1]) for i in range(0,len(args),2)]
+	return minuitLn.calcneglnL(retList)
+
+def initParsre(fileName):
+    kwdargs = {}  
+    for line in fileinput.input([dataFile]):
+        if line[0] != '#':
+            kvAs = line.split()
+            for kvAx in kvAs:
+                kwdargs[str(kvAx.split("=")[0])] = float(kvAx.split("=")[1])
+    return kwdargs 
 
 indir = sys.argv[2]
 Control = numpy.load(os.path.join(indir,"GUI","Control_List.npy"))
@@ -40,15 +52,19 @@ rAA = rhoAA(waves=waves,alphaList=alphaList,beamPolarization=float(Control[1]))
 rhoAA = rAA.calc()  
 numpy.save(os.path.join(dataDir,"data","rhoAA.npy"),rhoAA)
 minuitLn=FASTLikelihood(waves=waves,alphaList=alphaList,acceptedPath=acceptedPath,generatedPath=generatedPath,accNormInt=accNormInt,Q=QFactor,rhoAA=rhoAA)
-generator=fnGenerator.generator()
-generator.fileName=os.path.join(os.getcwd(),"generatedFn.py")
-generator.createFile(len(waves)*2)
-execfile(os.path.join(os.getcwd(),"generatedFn.py"))
-
-m.set_strategy(2)
-m.set_up(0.5)
-m.migrad(ncall=int(Control[6]))
-
+if not os.path.isfile(os.path.join(sys.argv[2],"scripts","minInit.txt")):
+    kwdarg = {"t"+str(i):.01 for i in range(len(waves)*2)}
+    kwdarg['errordef']=0.5
+    m=Minuit(minfn,**kwdarg)
+    m.set_strategy(2)
+    m.migrad(ncall=int(Control[6]))
+else:
+    kwdarg = initParser(os.path.join(sys.argv[2],"scripts","minInit.txt"))
+    kwdarg['errordef']=0.5
+    m=Minuit(minfn,**kwdarg)
+    m.set_strategy(2)
+    m.migrad(ncall=int(Control[6]))
+    
 Vvalues = m.values
 numpy.save(os.path.join(dataDir,"Vvalues.npy"),Vvalues)
 covariance=numpy.array(m.matrix())
