7771878a01803f5b259e436fc5de3cb9f6661715
Mark Jones <maj@jlab.org>
2015-10-15 16:59:50 -0400

    Finalized data Interface.

--- PyPWA/data/__init__.py
+++ PyPWA/data/__init__.py
@@ -18,26 +18,43 @@ class Interface(object):
 
     cache = True
 
-    files_to_load = {}
-
     def __init__(self, config = None):
         if config != None:
             self.cache = config["Use Cache"]
-            self.files_to_load = config["Files"]
 
 
     def extension_test(self, file_location ):
         try:
-            handlers = self.supported_file_types[os.path.splitext(file_location)[1]][1]
+            handlers = self.supported_file_types[os.path.splitext(file_location)[1]]
         except KeyError:
             raise TypeError("{0} is not a supported file extension".format(os.path.splitext(file_location)[1]))
 
         return handlers
 
     def parse(self, file_location, data_type = None):
-        if data_type == None:
+        if type(data_type) == type(None):
+            print True
+            handlers = self.extension_test(file_location)
+        else:
+            print False
+            handlers = data_type
+
+        print handlers
+
+        if handlers == "Kv":
+            handle = PyPWA.data.filehandling.Kv()
+            return handle.parse(file_location)
+
+    def write(self, file_location, data, data_type = None):
+        if type(data_type) == type(None):
             handlers = self.extension_test(file_location)
         else:
             handlers = data_type
 
-        #if handlers == "Kv":
+        if handlers == "Kv":
+            handle = PyPWA.data.filehandling.Kv()
+            handle.data = data
+            handle.write(file_location)
+
+
+
