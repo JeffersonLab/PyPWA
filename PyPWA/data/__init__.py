3beda3e1ccd0893a758db2873eb8cfd94151c8f2
Mark Jones <maj@jlab.org>
2015-10-15 11:55:02 -0400

    Added interface for file handlers

--- PyPWA/data/__init__.py
+++ PyPWA/data/__init__.py
@@ -10,9 +10,11 @@ __maintainer__ = "Mark Jones"
 __email__ = "maj@jlab.org"
 __status__ = "[CURRENT_STATUS]"
 
-class Interface(object)
+import PyPWA.data.filehandling, os
 
-    supported_file_types = { "Kv": ".txt" }
+class Interface(object):
+
+    supported_file_types = { ".txt" : "Kv" }
 
     cache = True
 
@@ -21,4 +23,21 @@ class Interface(object)
     def __init__(self, config = None):
         if config != None:
             self.cache = config["Use Cache"]
-            self.files_to_load = 
+            self.files_to_load = config["Files"]
+
+
+    def extension_test(self, file_location ):
+        try:
+            handlers = self.supported_file_types[os.path.splitext(file_location)[1]][1]
+        except KeyError:
+            raise TypeError("{0} is not a supported file extension".format(os.path.splitext(file_location)[1]))
+
+        return handlers
+
+    def parse(self, file_location, data_type = None):
+        if data_type == None:
+            handlers = self.extension_test(file_location)
+        else:
+            handlers = data_type
+
+        #if handlers == "Kv":
