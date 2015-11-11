f71f26bafb664a141023fea77908a80e2ef2969d
Mark Jones <maj@jlab.org>
2015-11-11 13:00:03 -0500

    Added numpy as a requirement for all modules in PyPWA

---
+++ PyPWA/__init__.py
@@ -0,0 +1,13 @@
+"""
+PyPWA/__init__.py: Initializer for all of PyPWA
+"""
+
+__author__ = "Mark Jones"
+__credits__ = ["Mark Jones"]
+__license__ = "MIT"
+__version__ = "2.0.0"
+__maintainer__ = "Mark Jones"
+__email__ = "maj@jlab.org"
+__status__ = "Alpha.2"
+
+import numpy
