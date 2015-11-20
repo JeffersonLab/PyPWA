f77b9388bb3767defaeb7c8f0f42670aeb23efd2
Mark Jones <maj@jlab.org>
2015-11-20 10:55:00 -0500

    Refactored for the renames in file_handler and console_main

--- PyPWA/entry/console.py
+++ PyPWA/entry/console.py
@@ -12,7 +12,7 @@ __maintainer__ = "Mark Jones"
 __email__ = "maj@jlab.org"
 __status__ = "[CURRENT_STATUS]"
 
-import  click, PyPWA.data.filehandler, os, PyPWA.core.console
+import  click, PyPWA.data.file_manager, os, PyPWA.core.console_main
 
 @click.command()
 @click.argument( "configuration", nargs=-1, type=click.Path(exists=True))
@@ -26,18 +26,18 @@ def start_console_general_fitting(configuration, writeconfig):
         click.secho("Use \"GeneralFitting --help\" for the proper way to use the utility", bold=True)
     else:
         if writeconfig:
-            config = PyPWA.core.console.Configurations()
+            config = PyPWA.core.console_main.Configurations()
             with open(os.getcwd() + "/Example.yml", "w") as stream:
                 stream.write(config.fitting_config)
             with open(os.getcwd() + "/Example.py", "w") as stream:
                 stream.write(config.example_function)
         else:
-            the_data = PyPWA.data.filehandler.MemoryInterface()
+            the_data = PyPWA.data.file_manager.MemoryInterface()
             the_config = the_data.parse(configuration[0])
             cwd = os.getcwd()
             click.clear()
             
-            fitting = PyPWA.core.console.Fitting(the_config, cwd)
+            fitting = PyPWA.core.console_main.Fitting(the_config, cwd)
             fitting.start()
 
 @click.command()
@@ -52,7 +52,7 @@ def start_console_general_simulator(configuration, writeconfig):
         click.secho("Use \"GeneralSimulator --help\" for the proper way to use the utility", bold=True)
     else:
         if writeconfig:
-            config = PyPWA.core.console.Configurations()
+            config = PyPWA.core.console_main.Configurations()
             with open(os.getcwd() + "/Example.yml", "w") as stream:
                 stream.write(config.simulator_config)
             with open(os.getcwd() + "/Example.py", "w") as stream:
@@ -63,5 +63,5 @@ def start_console_general_simulator(configuration, writeconfig):
             cwd = os.getcwd()
             click.clear()
             
-            simulating = PyPWA.core.console.Simulator(the_config, cwd)
+            simulating = PyPWA.core.console_main.Simulator(the_config, cwd)
             simulating.start()
