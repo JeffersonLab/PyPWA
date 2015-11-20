2a50598443fa4e50884efa82556feef465948119
Mark Jones <maj@jlab.org>
2015-11-20 06:08:03 -0500

    Changed public variables and methods to protected so they can't be modified externally. Changed how data was being passed around in bewteen methods in an attempt to be more resource conservative and human readable.

--- PyPWA/proc/calculation_tools.py
+++ PyPWA/proc/calculation_tools.py
@@ -14,90 +14,82 @@ import iminuit, warnings, sys, numpy
 
 class Minimalizer(object):
 
-
     def __init__(self, calc_function, parameters, settings, strategy, set_up, ncall):
-        self.calc_function = calc_function
-        self.parameters = parameters
-        self.settings = settings
-        self.strategy = strategy
-        self.set_up = set_up
-        self.ncall = ncall
+        self._calc_function = calc_function
+        self._parameters = parameters
+        self._settings = settings
+        self._strategy = strategy
+        self._set_up = set_up
+        self._ncall = ncall
         
-
     def min(self):
-        self.minimal = iminuit.Minuit(self.calc_function, forced_parameters=self.parameters, **self.settings )
-        self.minimal.set_strategy(self.strategy)
-        self.minimal.set_up(self.set_up)
-        self.minimal.migrad(ncall=self.ncall)
+        minimal = iminuit.Minuit(self._calc_function, forced_parameters=self._parameters, **self._settings )
+        minimal.set_strategy(self._strategy)
+        minimal.set_up(self._set_up)
+        minimal.migrad(ncall=self._ncall)
+
+
 
 class FunctionLoading(object):
-    def __init__(self, cwd, function_location, function_name, setup_name = None ):
-        self.cwd = cwd
-        self.function_location = function_location
-        self.function_name = function_name
-        self.setup_name = setup_name
+    def __init__(self, cwd, function_location, function_name, setup_name ):
 
-        self.__import_function()
+        self._users_amplitude, self._users_setup = self._import_function(cwd, function_location, function_name, setup_name)
 
-    def _import_function(self):
-        sys.path.append(self.cwd)
+
+    def _import_function(self, cwd, function_location, function_name, setup_name):
+        sys.path.append(cwd)
         try:
-            imported = __import__(self.function_location.strip(".py"))
+            imported = __import__(function_location.strip(".py"))
         except ImportError:
             raise 
 
         try:
-            self.users_function = getattr(imported, self.function_name)
-        except AttributeError:
-            raise ValueError("There is not function named {0} in the function file!".format(self.function_name))
+            users_amplitude = getattr(imported, function_name)
+        except:
+            raise
 
         try:
-            self.setup_function = getattr(imported, self.setup_name)
+            setup_function = getattr(imported, setup_name)
         except AttributeError:
-            warnings.warn("Setup fucntion {0} was not found in {1}, going without setup function".format(self.setup_function, self.function_location ), UserWarning)
-            self.setup_function = empty_function
+            warnings.warn("Setup fucntion {0} was not found in {1}, going without setup function".format(setup_function, function_location ), UserWarning)
+            def empty(): pass
+            setup_function = empty
 
-    @staticmethod
-    def empty_function():
-        pass
+        return [ users_amplitude, setup_function ]
 
     def return_amplitude(self):
-        return self.users_function
+        return self._users_function
 
     def return_setup(self):
-        return self.setup_function
+        return self._users_setup
+
+
 
 class DataChunking(object):
-
-    _num_chunks = 0
-    _data = None
-    _split_data = None
 
     def chunks(self, data, num_chunks):
         if num_chunks == 1:
             return [data]
 
-        self._num_chunks = num_chunks
-        self._data = data
-
-        if type(self._data) == dict:
-            self._dictionary_split()
-        elif type(self._data) == numpy.ndaray:
-            self._array_split()
+        if type(data) == dict:
+            return self._dictionary_split(data, num_chunks)
+        elif type(data) == numpy.ndaray:
+            return self._array_split(data, num_chunks)
 
         return self._split_data
 
 
-    def _dictionary_split(self):
-        self._split.data = []
+    def _dictionary_split(self, dictionary, num_chunks):
+        split_dictionary = []
         
-        for x in range(self._num_chunks):
-            self._split_data.append({})
+        for x in range(num_chunks):
+            split_dictionary.append({})
 
-        for key in self._data:
-            for index in range(self._num_chunks):
-                self._split_data[index][key] = numpy.array_split(self._data[key],(self._num_chunks))[index]
+        for key in dictionary:
+            for index in range(num_chunks):
+                split_dictionary[index][key] = numpy.array_split(data[key],(num_chunks))[index]
+        return split_dictionary
 
 
-    def _array_split(self):
-        self._split_data = numpy.array_split(self._data, self._num_chunks)
+    def _array_split(self, array, num_chunks):
+        return numpy.array_split(array, num_chunks)
