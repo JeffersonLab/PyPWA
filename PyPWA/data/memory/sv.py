5d185409b61df54960d9a2f7d8d3d85707126204
Mark Jones <maj@jlab.org>
2015-12-03 14:10:03 -0500

    Initial work for Variable Separated files. Supports reading .tsv and .csv files. Can not write back yet.

---
+++ PyPWA/data/memory/sv.py
@@ -0,0 +1,35 @@
+import csv
+import fileinput
+import numpy
+
+
+class SvParser(object):
+    def __init__(self, delimiter):
+        self.delimiter = delimiter
+
+    def reader(self, file_location):
+        for line_count, throw in enumerate(fileinput.input(file_location)):
+            pass
+
+        with open(file_location, "rt") as stream:
+            sv = csv.reader(stream, delimiter=self.delimiter)
+
+            elements = sv.next()
+            parsed = {}
+            for element in elements:
+                parsed[element] = numpy.zeros(shape=line_count, dtype="float64")
+
+            for index, row in enumerate(sv):
+                for count in range(len(row)):
+                    parsed[elements[count]][index] = row[count]
+
+        return parsed
+
+    def writer(self, file_location, data):
+
+        if self.delimiter == "\t":
+            lect = "excel-tab"
+        else:
+            lect = "excel"
+
+        raise NotImplementedError("Writing of Variable Seperated Values is unsupported at this time")
