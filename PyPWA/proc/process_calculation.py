cae3c529bd6c2189954053fae02366a7d76e2f8c
Mark Jones <maj@jlab.org>
2015-12-04 15:08:41 -0500

    Split Likelihood function into two objects, Extended and Unextended. Both support Bins if included in the data.

--- PyPWA/proc/process_calculation.py
+++ PyPWA/proc/process_calculation.py
@@ -10,13 +10,15 @@ __email__ = "maj@jlab.org"
 __status__ = "Beta0"
 
 from abc import ABCMeta, abstractmethod
-import multiprocessing, numpy
+import multiprocessing
+import numpy
+
 
 class AbstractProcess(multiprocessing.Process):
-    """Abstract class extendeding multiprocessing.Process
+    """Abstract class extending multiprocessing.Process
     Attributes:
-        _looping (optional[bool]): Default True. Defines if if to continue _looping
-        daemon (optional[bool]): Default True. If process is bool
+        _looping  Defines if if to continue _looping, is set to true
+        daemon   If process is bool, is set to true.
     """
     __metaclass__ = ABCMeta
 
@@ -24,7 +26,7 @@ class AbstractProcess(multiprocessing.Process):
     daemon = True
 
     def __init__(self):
-        super(multiprocessing.Process, self).__init__()
+        super(AbstractProcess, self).__init__()
 
     def run(self):
         """Main loop for Processes"""
@@ -47,19 +49,19 @@ class AbstractProcess(multiprocessing.Process):
         pass
 
 
-class RejctionAcceptanceAmplitude(AbstractProcess):
+class RejectionAcceptanceAmplitude(AbstractProcess):
     """Acceptance Rejection Process
     Args:
-        amplitude_function (object): Users amplitude function
-        setup_function (object): Setup function
+        amplitude_function (function): Users amplitude function
+        setup_function (function): Setup function
         data (dict): Dictionary of numpy arrays
         parameters (dict): Parameters used to calculate
         send (Pipe): Pipe to use to send data back to the main thread
         the_id (int): id of the thread
     """
 
-    def __init__(self, amplitude_function, setup_function, data, parameters, send, the_id ):
-        super(AbstractProcess, self).__init__()
+    def __init__(self, amplitude_function, setup_function, data, parameters, send, the_id):
+        super(RejectionAcceptanceAmplitude, self).__init__()
         self._amplitude_function = amplitude_function
         self._setup_function = setup_function
         self._data = data
@@ -67,74 +69,115 @@ class RejctionAcceptanceAmplitude(AbstractProcess):
         self._send = send
         self._id = the_id
 
-
     def setup(self):
-        """Runs the setup fucntion"""
+        """Runs the setup function"""
         self._setup_function()
-
 
     def _pipe_send(self, data):
         """Sends the data back to the main thread"""
         self._send.send(data)
 
-
     def processing(self):
         """Processes data"""
-        self._pipe_send([ self._id, self._amplitude_function(self._data, self._parameters)])
+        self._pipe_send([self._id, self._amplitude_function(self._data, self._parameters)])
         self._looping = False
 
 
-class LikelihoodAmplitude(AbstractProcess):
-    """Liklihood Estimation Process
+class AbstractLikelihoodAmplitude(AbstractProcess):
+    """Abstract Likelihood that handles all the processing except the likelihood
+    Attributes:
+        setup_function (function): the function to run before calculation
+        send (multiprocessing.Pipe): The pipe to send information back with
+        receive (multiprocessing.Pipe): The pipe to receive parameters back from
+    """
+
+    def __init__(self, setup_function, send, receive):
+        super(AbstractLikelihoodAmplitude, self).__init__()
+        self._setup_function = setup_function
+        self._send = send
+        self._receive = receive
+
+    def setup(self):
+        """Runs the setup function"""
+        self._setup_function()
+
+    def _pipe_send(self, data):
+        """Handles sending data over pipe"""
+        self._send.send(data)
+
+    def _pipe_receive(self):
+        """Handles receiving data from pipe"""
+        return self._receive.recv()
+
+    def processing(self):
+        """Processes the data"""
+        parameters = self._pipe_receive()
+        if parameters == "DIE":
+            self._looping = False
+        else:
+            result = self.likelihood(parameters)
+            self._pipe_send(result)
+
+    @abstractmethod
+    def likelihood(self, parameters):
+        pass
+
+
+class ExtendedLikelihoodAmplitude(AbstractLikelihoodAmplitude):
+    """Likelihood Estimation Process
     Args:
-        amplitude_function (object): Users amplitude function
-        setup_function (object): Setup function
+        amplitude_function (function): Users amplitude function
+        setup_function (function): Setup function
         processed (float): Constant for likelihood, 1/Generated_length
         data (dict): Dictionary of numpy arrays
         accepted (dict): Dictionary of numpy arrays with Accepted Monte Carlo
         send (Pipe): Pipe to use to send data back to the main thread
-        recieve (Pipe): Pipe to receive parameters with
+        receive (Pipe): Pipe to receive parameters with
     """
 
-    def __init__(self, amplitude_function, setup_function, processed, data, accepted, qfactor, send, recieve ):
-        super(AbstractProcess, self).__init__()
+    def __init__(self, amplitude_function, setup_function, processed, data, accepted, send, receive):
+        super(ExtendedLikelihoodAmplitude, self).__init__(setup_function, send, receive)
         self._amplitude_function = amplitude_function
-        self._setup_function = setup_function
         self._processed = processed
         self._data = data
         self._accepted = accepted
-        self._qfactor = qfactor
-        self._send = send
-        self._recieve = recieve
-
-
-    def setup(self):
-        """Runs the setup fucntion"""
-        self._setup_function()
-
-
-    def _pipe_send(self, data):
-        """Handles sending data over pipe"""
-        self._send.send((data))
-
-
-    def _pipe_recieve(self):
-        """Handles recieving data from pipe"""
-        return self._recieve.recv()
-
 
     def processing(self):
-        """Processes the data"""
-        parameters = self._pipe_recieve()
-        if parameters == "DIE":
-            self._looping = False
-        else:
-            result = self._likelihood(parameters)
-            self._pipe_send(result)
+        super(ExtendedLikelihoodAmplitude, self).processing()
+
+    def setup(self):
+        super(ExtendedLikelihoodAmplitude, self).setup()
+
+    def likelihood(self, parameters):
+        """Calculates the likelihood function
+        Args:
+            parameters (dict): dictionary of the arguments to be sent to the function
+        """
+        processed_data = self._amplitude_function(self._data["data"], parameters)
+        processed_accepted = self._amplitude_function(self._accepted["data"], parameters)
+        return -(numpy.sum(self._data["QFactor"] * self._data["BinN"] * numpy.log(processed_data))) + \
+                (self._processed * self._accepted["BinN"] * numpy.sum(processed_accepted))
 
 
-    def _likelihood(self, parameters):
-        """Calculates the likelihood function"""
-        processed_data = self._amplitude_function( self._data, parameters )
-        processed_accepted = self._amplitude_function( self._accepted, parameters )
-        return -(numpy.sum(self._qfactor * numpy.log(processed_data))) + self._processed * numpy.sum(processed_accepted)
+class UnextendedLikelihoodAmplitude(AbstractLikelihoodAmplitude):
+    """Likelihood Estimation Process
+    Args:
+        amplitude_function (function): Users amplitude function
+        setup_function (function): Setup function
+        data (dict): Dictionary of numpy arrays
+        send (Pipe): Pipe to use to send data back to the main thread
+        receive (Pipe): Pipe to receive parameters with
+    """
+
+    def __init__(self, amplitude_function, setup_function, data, send, receive):
+        super(UnextendedLikelihoodAmplitude, self).__init__(setup_function, send, receive)
+        self._amplitude_function = amplitude_function
+        self._data = data
+
+    def likelihood(self, parameters):
+        """Calculates the likelihood function
+        Args:
+            parameters (dict): dictionary of the arguments to be sent to the function
+        """
+        processed_data = self._amplitude_function(self._data["data"], parameters)
+        return -(numpy.sum(self._data["QFactor"] * self._data["BinN"] * numpy.log(processed_data)))
