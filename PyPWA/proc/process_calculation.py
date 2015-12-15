b60336d18e1dbdf3c8a599eebbf72db7b8cdc86a
Mark Jones <maj@jlab.org>
2015-12-15 12:14:09 -0500

    Fixed bug in UnextendedLikelihoodAmplitude that would result in the wrong answer being returned.

--- PyPWA/proc/process_calculation.py
+++ PyPWA/proc/process_calculation.py
@@ -180,6 +180,7 @@ class UnextendedLikelihoodAmplitude(AbstractLikelihoodAmplitude):
             if self._data["BinN"][index] == 0:
                 pass
             else:
-                value += self._data["QFactor"][index] * self._data["BinN"][index] * numpy.log(processed_data[index])
+                value += -(numpy.sum(self._data["QFactor"][index] * self._data["BinN"][index] *
+                                     numpy.log(processed_data[index])))
 
         return -value
