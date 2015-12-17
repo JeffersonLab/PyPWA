6a5337978c5ccede6b5c6c7ca0378a1af069b644
Mark Jones <maj@jlab.org>
2015-12-17 13:31:23 -0500

    Converted tests to pytest

--- tests/proc/test_process_communication.py
+++ tests/proc/test_process_communication.py
@@ -1,15 +1,13 @@
-import unittest
 import PyPWA.proc.process_communication
 
 
-class TestProcessPipes(unittest.TestCase):
-    def test(self):
-        send_pipes, receive_pipes = PyPWA.proc.process_communication.ProcessPipes.return_pipes(24)
+def test_process_pipes():
+    send_pipes, receive_pipes = PyPWA.proc.process_communication.ProcessPipes.return_pipes(24)
 
-        the_list = range(100, 2500, 100)
+    the_list = range(100, 2500, 100)
 
-        for index, value in enumerate(the_list):
-            send_pipes[index].send(value)
+    for index, value in enumerate(the_list):
+        send_pipes[index].send(value)
 
-        for index, value in enumerate(the_list):
-            self.assertEquals(receive_pipes[index].recv(), value)
+    for index, value in enumerate(the_list):
+        assert receive_pipes[index].recv() == value
