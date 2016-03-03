67a6e06a936490376bc51e1a7caccad0124c0090
Mark Jones <maj@jlab.org>
2016-03-03 14:53:18 -0500

    Corrected spelling.

--- PyPWA/libs/data/__init__.py
+++ PyPWA/libs/data/__init__.py
@@ -3,7 +3,7 @@ This module loads data from various data types to be used
 inside the program as they would like. Data types supported
 are the classic Kinematic Variable files defined by Josh
 Pond, the QFactors List, the Weight list, the Condensed
-single line Weight, and the Tab or Comma seperated
+single line Weight, and the Tab or Comma separated
 kinematic variables.
 
 Examples:
