3efa63ef4720b73511b6e505448749ccd04058d9
Mark Jones <maj@jlab.org>
2016-04-11 15:47:29 -0400

    Refactored delimiter based files, added support for writing CSV and TSV to and from disk.

--- PyPWA/libs/data/memory/sv.py
+++ PyPWA/libs/data/memory/sv.py
@@ -1,4 +1,3 @@
-
 import csv
 import io
 
@@ -16,16 +15,27 @@ __version__ = VERSION
 
 
 class SvParser(object):
-    def __init__(self, delimiter):
-        self.delimiter = delimiter
+    """
+    Object for reading and writing delimiter separated data files.
+    """
 
     def reader(self, file_location):
+        """Reads a delimiter separated file containing data from disk.
+
+        Args:
+            file_location (str): The file that contain the data to be read.
+        Returns:
+            dict: Dictionary of arrays containing the data.
+        """
         with io.open(file_location, "rt") as stream:
             for line_count, throw in enumerate(stream):
                 pass
 
         with io.open(file_location, "rt") as stream:
-            sv = csv.reader(stream, delimiter=self.delimiter)
+            dialect = csv.Sniffer().sniff(stream.read(1024))
+            stream.seek(0)
+
+            sv = csv.reader(stream, dialect)
 
             elements = next(sv)
             parsed = {}
@@ -39,4 +49,27 @@ class SvParser(object):
         return parsed
 
     def writer(self, file_location, data):
-        raise NotImplementedError("Writing of Variable Separated Values is unsupported at this time")
+        """Writes the data from memory to file.
+
+        Args:
+            file_location  (str): Where to write the data to.
+            data (dict): Dictionary of the arrays containing the data.
+        """
+        extension = file_location.split(".")[-1]
+
+        if extension == ".tsv":
+            dialect = csv.excel_tab
+        else:
+            dialect = csv.excel
+
+        with io.open(file_location, "wt") as stream:
+            field_names = list(data.keys())
+
+            writer = csv.DictWriter(stream, fieldnames=field_names, dialect=dialect)
+            writer.writeheader()
+
+            for index in range(len(data[field_names[0]])):
+                temp = {}
+                for field in field_names:
+                    temp[field] = data[field][index]
+                writer.writerow(temp)
