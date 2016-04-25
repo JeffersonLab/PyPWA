c80f06e80eb3c3b6d51c6a1977fef8c108b11f60
Mark Jones <maj@jlab.org>
2016-04-25 14:48:09 -0400

    Added documentation to configuratr along with logic that will lower case all the keys in the dictionary.

--- PyPWA/libs/configuratr.py
+++ PyPWA/libs/configuratr.py
@@ -16,25 +16,36 @@ __version__ = VERSION
 
 
 class DataConfiguratr(object):
+    """Stores the parsed Data Configuration
+    This object stores the configuration data for the data in a way
+    that can be applied for all configuration throughout the program.
+    This could probably be made in a different way that would be more
+    efficient, ie tuples, but this does the job for now and ultimately
+    shouldn't be initialized more than a few times throughout the
+    existence of a single operation.
+
+    Args:
+        data (dict) : The dictionary containing the configuration loaded from file.
+    """
     def __init__(self, data):
         self.logger = logging.getLogger(__name__)
         self.logger.addHandler(logging.NullHandler)
-        self._data = data
-        self._parse_data()
+        self._parse_data(data)
         self._save_location = False
         self._accepted_location = False
         self._data_location = False
         self._qfactor_location = False
 
-    def _parse_data(self):
-        if "save name" in self._data:
-            self._parse_save_location(self._data["save name"])
-        if "data location" in self._data:
-            self._parse_data_location(self._data["data location"])
-        if "accepted monte carlo location" in self._data:
-            self._parse_accepted_location(self._data["accepted monte carlo location"])
-        if "qfactor list location" in self._data:
-            self._parse_qfactor_location(self._data["qfactor list location"])
+    def _parse_data(self, data):
+        new_data = {k.lower(): v for k, v in data.items()}  # This lower cases just the keys in the dictionary.
+        if "save name" in new_data:
+            self._parse_save_location(new_data["save name"])
+        if "data location" in new_data:
+            self._parse_data_location(new_data["data location"])
+        if "accepted monte carlo location" in new_data:
+            self._parse_accepted_location(new_data["accepted monte carlo location"])
+        if "qfactor list location" in new_data:
+            self._parse_qfactor_location(new_data["qfactor list location"])
 
     def _parse_save_location(self, data):
         if isinstance(data, str) or isinstance(data, list):
@@ -81,5 +92,5 @@ class DataConfiguratr(object):
         return self._data_location
 
 
-class MinuitConfiguratr(object):
-    def __init__(self):
+#class MinuitConfiguratr(object):
+#    def __init__(self):
