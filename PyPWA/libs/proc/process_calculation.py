1d9a3ede37fe33b0bd08380d9ae79208b1d68f05
Mark Jones <markjonestx@live.com>
2016-05-10 00:08:45 -0400

    Added the Factories needed to build the processes.

--- PyPWA/libs/proc/process_calculation.py
+++ PyPWA/libs/proc/process_calculation.py
@@ -2,6 +2,7 @@
 Actual process objects are defined here
 """
 import multiprocessing
+from PyPWA.libs.proc import process_communication
 
 __author__ = "Mark Jones"
 __credits__ = ["Mark Jones"]
@@ -26,6 +27,8 @@ class DuplexProcess(multiprocessing.Process):
             value = self.communication.recv()
             if value == "DIE":
                 break
+            elif value == "IGNORE":
+                pass
             else:
                 self.communication.send(self.kernel.process(value))
         return 0
@@ -43,3 +46,43 @@ class SimplexProcess(multiprocessing.Process):
         self.kernel.setup()
         self.communication.send(self.kernel.process())
         return 0
+
+
+class SimplexCalculationFactory(object):
+    def __init__(self, kernel, count):
+        self._kernel = kernel
+        self._count = count
+        self._processes = []
+        self._receives = []
+
+    def build(self):
+        sends, self._receives = process_communication.SimplexFactory(self._count)
+
+        for kernel, send in zip(self._kernel, sends):
+            self._processes.append(SimplexProcess(kernel, send))
+
+        return self.processed
+
+    @property
+    def processed(self):
+        return [self._processes, self._receives]
+
+
+class DuplexCalculationFactory(object):
+    def __init__(self, kernel, count):
+        self._kernel = kernel
+        self._count = count
+        self._processes = []
+        self._main_com = []
+
+    def build(self):
+        self._main_com, process_com = process_communication.DuplexFactory(self._count)
+
+        for kernel, process_com in zip(self._kernel, process_com):
+            self._processes.append(DuplexProcess(kernel, process_com))
+
+        return self.processed
+
+    @property
+    def processed(self):
+        return [self._processes, self._main_com]
