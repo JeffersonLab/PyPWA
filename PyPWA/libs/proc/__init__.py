da417ca59381d74463d3edcb382715171a6dda27
Mark Jones <markjonestx@live.com>
2016-05-10 00:08:04 -0400

    Touched up documentation and added imports.

--- PyPWA/libs/proc/__init__.py
+++ PyPWA/libs/proc/__init__.py
@@ -1,1 +1,14 @@
-"""Module is in charge of calculation of data using multiprocessing"""
+"""
+This is the builtin plugin for Multiprocessing, it makes a handful of assumptions but it works.
+"""
+from PyPWA.libs.proc.calculation import CalculationForeman
+from PyPWA import VERSION, LICENSE, STATUS
+
+__author__ = ["Mark Jones"]
+__credits__ = ["Mark Jones"]
+__maintainer__ = ["Mark Jones"]
+__email__ = "maj@jlab.org"
+__status__ = STATUS
+__license__ = LICENSE
+__version__ = VERSION
+
