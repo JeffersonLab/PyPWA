6fc60ff4a133e9b6039589d3b6bfcb99fe79f8a2
Mark Jones <markjonestx@live.com>
2016-05-11 15:08:48 -0400

    Added abstract objects for the kernels. These will be flushed out in time,

--- PyPWA/libs/process/utilities.py
+++ PyPWA/libs/process/utilities.py
@@ -1,6 +1,9 @@
 """
 Tools needed for the various Amplitude analysing utilities.
 """
+
+import numpy
+
 __author__ = "Mark Jones"
 __credits__ = ["Mark Jones"]
 __license__ = "MIT"
@@ -9,110 +12,13 @@ __maintainer__ = "Mark Jones"
 __email__ = "maj@jlab.org"
 __status__ = "Beta0"
 
-import iminuit
-import numpy
-import sys
-import warnings
+
+class AbstractInterface(object):
+    pass
 
 
-# TODO move this worthless pile of code.
-
-
-class Minimizer(object):
-    """Object based off of iminuit, provides an easy way to run minimalization
-    Args:
-        calc_function (function): function that holds the calculations.
-        parameters (list): List of the parameters
-        settings (dict): Dictionary of the settings for iminuit
-        strategy (int): iminuit's strategy
-        set_up (int): Todo
-        ncall (int): Max number of calls
-    """
-
-    def __init__(self, calc_function, parameters, settings, strategy, set_up, ncall):
-        self.fval = 0
-        self.covariance = 0
-        self.values = 0
-        self._calc_function = calc_function
-        self._parameters = parameters
-        self._settings = settings
-        self._strategy = strategy
-        self._set_up = set_up
-        self._ncall = ncall
-
-    def min(self):
-        """Method to call to start minimization process"""
-        minimal = iminuit.Minuit(self._calc_function, forced_parameters=self._parameters, **self._settings )
-        minimal.set_strategy(self._strategy)
-        minimal.set_up(self._set_up)
-        minimal.migrad(ncall=self._ncall)
-        self.fval = minimal.fval
-        self.covariance = minimal.covariance
-        self.values = minimal.values
-
-
-class FunctionLoading(object):
-    """Object that loads the user defined functions from file
-    Args:
-        cwd (str): Path to folder with the functions
-        function_location (str): Path to the file
-        function_name (str): Name of Amplitude function
-        setup_name (str): Name of Setup function.
-    """
-
-    def __init__(self, cwd, function_location, function_name, setup_name):
-        self._users_amplitude, self._users_setup = self._import_function(cwd, function_location, function_name,
-                                                                         setup_name)
-
-    @staticmethod
-    def _import_function(cwd, function_location, function_name, setup_name):
-        """Imports and sets up functions for usage.
-        Args:
-            cwd (str): Path to folder with the functions
-            function_location (str): Path to the file
-            function_name (str): Name of Amplitude function
-            setup_name (str): Name of Setup function.
-        Returns:
-            list: [ amplitude function, setup function ]
-        """
-        sys.path.append(cwd)
-        try:
-            imported = __import__(function_location.strip(".py"))
-        except ImportError:
-            raise
-
-        try:
-            users_amplitude = getattr(imported, function_name)
-        except:
-            raise
-
-        try:
-            setup_function = getattr(imported, setup_name)
-        except AttributeError:
-            warnings.warn(("Setup function  {0} was not found in {1},"
-                           "going without setup function").format(setup_name, function_location), UserWarning)
-
-            def empty():
-                pass
-            setup_function = empty
-
-        return [users_amplitude, setup_function]
-
-    @property
-    def return_amplitude(self):
-        """Returns amplitude
-        Returns:
-            function: Amplitude Function
-        """
-        return self._users_amplitude
-
-    @property
-    def return_setup(self):
-        """Returns setup
-        Returns:
-            function: Setup Function
-        """
-        return self._users_setup
+class AbstractProcess(object):
+    pass
 
 
 class DictionarySplitter(object):
