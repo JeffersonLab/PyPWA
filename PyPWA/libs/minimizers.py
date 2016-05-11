9b14f62c5137dc15c97318bf58cbcb4197d462d6
Mark Jones <maj@jlab.org>
2016-05-11 19:49:51 -0400

    Restructured the code of the iminuit plugin.

--- PyPWA/libs/minimizers.py
+++ PyPWA/libs/minimizers.py
@@ -1,8 +1,40 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2014-2016 JLab.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import iminuit
+
+from PyPWA import VERSION, LICENSE, STATUS
+
+__author__ = ["Mark Jones"]
+__credits__ = ["Mark Jones"]
+__maintainer__ = ["Mark Jones"]
+__email__ = "maj@jlab.org"
+__status__ = STATUS
+__license__ = LICENSE
+__version__ = VERSION
 
 
 class Minimizer(object):
-    """Object based off of iminuit, provides an easy way to run minimalization
+    """Object based off of iminuit, provides an easy way to run minimization
     Args:
         calc_function (function): function that holds the calculations.
         parameters (list): List of the parameters
@@ -12,8 +44,9 @@ class Minimizer(object):
         ncall (int): Max number of calls
     """
 
-    def __init__(self, calc_function, parameters, settings, strategy, set_up, ncall):
-        self.fval = 0
+    def __init__(self, calc_function, parameters, settings, strategy, set_up,
+                 ncall):
+        self.final_value = 0
         self.covariance = 0
         self.values = 0
         self._calc_function = calc_function
@@ -25,10 +58,14 @@ class Minimizer(object):
 
     def min(self):
         """Method to call to start minimization process"""
-        minimal = iminuit.Minuit(self._calc_function, forced_parameters=self._parameters, **self._settings )
+        minimal = iminuit.Minuit(
+            self._calc_function,
+            forced_parameters=self._parameters,
+            **self._settings )
+
         minimal.set_strategy(self._strategy)
         minimal.set_up(self._set_up)
         minimal.migrad(ncall=self._ncall)
-        self.fval = minimal.fval
+        self.final_value = minimal.fval
         self.covariance = minimal.covariance
         self.values = minimal.values
