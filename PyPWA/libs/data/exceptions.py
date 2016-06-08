688335106e2d6e4d5dae62be5cbfcef313d6e6d5
Mark Jones <maj@jlab.org>
2016-06-08 11:42:13 -0400

    Added template classes for the plugins.

--- PyPWA/libs/data/exceptions.py
+++ PyPWA/libs/data/exceptions.py
@@ -31,5 +31,80 @@ __license__ = LICENSE
 __version__ = VERSION
 
 
+class TemplateValidator(object):
+
+    def __init__(self, file_location, full=False):
+        self._file_location = file_location
+        self._full = full
+
+    def test(self):
+        raise NotImplementedError("%s does not overwrite method test. This is"
+                                  "the method that you should use to overwrite"
+                                  "to nest your individual tests into." %
+                                  self.__class__.__name__)
+
+
+class TemplateMemory(object):
+
+    def parse(self, file_location):
+        raise NotImplementedError("%s does not overwrite method parse. This is"
+                                  "the method that you should overwrite to have"
+                                  "the object read data into memory from disk."
+                                  % self.__class__.__name__)
+
+    def write(self, file_location, data):
+        raise NotImplementedError("%s does not overwrite method write. This is"
+                                  "the method that you should overwrite to have"
+                                  "the object write your data to disk from"
+                                  "memory" % self.__class__.__name__)
+
+
+class TemplateReader(object):
+
+    def __init__(self, file_location):
+        self._the_file = file_location
+
+    def reset(self):
+        raise NotImplementedError("%s does not overwrite method write. This is"
+                                  "the method that you should overwrite to have"
+                                  "the object reset properly when this method"
+                                  "is called." % self.__class__.__name__)
+
+    @property
+    def next_event(self):
+        raise NotImplementedError("%s does not overwrite method write. This is"
+                                  "the method that you should overwrite to have"
+                                  "the object read in the next event properly"
+                                  "when its called." % self.__class__.__name__)
+
+    def __next__(self):
+        self.next_event
+
+    def __iter__(self):
+        self.next_event
+
+
+class TemplateWriter(object):
+
+    def __init__(self, file_location):
+        self._the_file = file_location
+
+    def write(self, data):
+        raise NotImplementedError("%s does not overwrite method write. This is"
+                                  "the method that you should overwrite to have"
+                                  "the object write the data out to the disk"
+                                  "correctly." % self.__class__.__name__)
+
+    def close(self):
+        raise NotImplementedError("%s does not overwrite method write. This is"
+                                  "the method that you should overwrite to have"
+                                  "the object close the stream properly when "
+                                  "its called" % self.__class__.__name__)
+
+
 class IncompatibleData(Exception):
+    pass
+
+
+class UnknownData(Exception):
     pass
