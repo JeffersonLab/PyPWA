d6adcb7edd70c532ff97cafb6e116318d70c4b8e
Mark Jones <maj@jlab.org>
2016-06-09 15:52:45 -0400

    Added Option parser with yaml comments so that when they are rendered they will contain comments to help users understand their function. Started the Main Object.

--- PyPWA/libs/data/traffic_cop.py
+++ PyPWA/libs/data/traffic_cop.py
@@ -29,6 +29,7 @@ import ruamel.yaml
 import ruamel.yaml.comments
 
 from PyPWA.configuratr import tools
+from PyPWA.libs.data import MODULE_NAME
 from PyPWA.libs.data import definitions
 from PyPWA.libs.data import _utilites
 from PyPWA.libs.data import builtin
@@ -44,18 +45,117 @@ __version__ = VERSION
 
 BUILTIN_PACKAGE_LOCATION = builtin
 
-class Options(object):
-    cache = True
-    file_location = str
-    fail = False
 
-    def test_method(self):
-        data_settings = ruamel.yaml.comments.CommentedMap()
+class Options(object):
+    _options = {
+        "cache": True,  # Optional
+        "clear cache": False,  # Advanced
+        "fail": False,  # Advanced
+        "user plugin": "cwd=/path/to/file;"  # Advanced
+    }
+
+    _template = {
+        "cache": bool,
+        "clear cache": bool,
+        "fail": bool,
+        "user plugin": str
+    }
+
+    def __init__(self):
+        header = self._build_empty_options_with_comments()
+        self._optional = self._build_optional(header)
+        self._advanced = self._build_advanced(header)
+        self._required = ruamel.yaml.comments.CommentedMap()
+
+    @staticmethod
+    def _build_empty_options_with_comments():
+        header = ruamel.yaml.comments.CommentedMap()
+        content = ruamel.yaml.comments.CommentedMap()
+
+        header[MODULE_NAME] = content
+        header.yaml_add_eol_comment('This is the builtin data parser, you can '
+                                    'replace this with your own data parser if '
+                                    'you wish.', MODULE_NAME)
+
+        content.yaml_add_eol_comment("Should Cache be enabled? The cache will"
+                                     "automatically clear if it detects a"
+                                     "change in any of your data and should be"
+                                     "safe to leave enabled.", "cache")
+        content.yaml_add_eol_comment("Should we force the cache to clear? This"
+                                     "will destroy all of your caches, this"
+                                     "means loading your data will take much"
+                                     "longer, its recommended to leave this off"
+                                     "unless you are certain its a cache "
+                                     "issue.", "clear cache")
+        content.yaml_add_eol_comment("Should the program stop if it fails to "
+                                     "load the file? The program will already"
+                                     "fail if the data is needed for parsing"
+                                     "to happen, if this is set to true even"
+                                     "files that are optional will cause the "
+                                     "program to stop.", "fail")
+        content.yaml_add_eol_comment("A plugin that can be loaded into the "
+                                     "the " + MODULE_NAME + " for parsing,"
+                                     " see the documentation on the " +
+                                     MODULE_NAME + " plugin for more "
+                                     "information.", "user plugin")
+        return header
+
+    def _build_optional(self, header):
+        header[MODULE_NAME]["cache"] = self._options["cache"]
+        return header
+
+    def _build_advanced(self, header):
+        header = self._build_optional(header)
+        header[MODULE_NAME]["clear cache"] = self._options["clear cache"]
+        header[MODULE_NAME]["fail"] = self._options["fail"]
+        return header
+
+    @property
+    def return_template(self):
+        return self._template
+
+    @property
+    def return_required(self):
+        return self._required
+
+    @property
+    def return_optional(self):
+        return self._optional
+
+    @property
+    def return_advanced(self):
+        return self._advanced
+
+    @property
+    def return_defaults(self):
+        return self._options
+
+
+class _DataInterface(object):
+
+    def __init__(self):
+        self._is_ready = False
+        self._data_type = None  # type: str
+
+    @property
+    def is_ready(self):
+        return self._is_ready
+
+    @property
+    def wait(self):
+        raise NotImplementedError(str(self.__class__.__name__) + " does not "
+                                  "implement the wait property")
 
 
 class TrafficCop(object):
 
-    def __init__(self, external_packages=False, global_settings=False):
+    def __init__(self, settings):
+        fixer = tools.CorrectSettings()
+        options = Options()
+        corrected_settings = fixer.correct_dictionary(settings,
+                                                      options.return_template)
+
+
         if isinstance(external_packages, str):
             packages = [external_packages, BUILTIN_PACKAGE_LOCATION]
         elif isinstance(external_packages, list):
@@ -63,19 +163,11 @@ class TrafficCop(object):
         else:
             packages = [BUILTIN_PACKAGE_LOCATION]
 
-        if isinstance(global_settings, dict):
-            global_settings = tools.lowercase_dict(global_settings)
-            try:
-                global_settings["cache"]
-
-
         plugins = self._index_packages(packages)
         self._search = _utilites.DataSearch(plugins)
 
         self._logger = logging.getLogger(__name__)
         self._logger.addHandler(logging.NullHandler())
-
-
 
     @staticmethod
     def _index_packages(packages):
@@ -86,5 +178,4 @@ class TrafficCop(object):
 
         return plugins
 
-    def reader(self, file_location, event_iterator=False):
-        the_plugin = self._search.search(file_location)
+
