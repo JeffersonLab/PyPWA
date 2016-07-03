7513d4e9c5ca6d841ec411c04077e16a60237298
Mark Jones <maj@jlab.org>
2016-07-03 17:51:07 -0400

    Cleaned up code a small  bit.

--- PyPWA/unoptimized/pythonPWA/fileHandlers/bampReader.py
+++ PyPWA/unoptimized/pythonPWA/fileHandlers/bampReader.py
@@ -32,19 +32,21 @@ def read_bamp(filename):
     This is a general bamp reading function.
 
     Args:
-    filename (string): Path to bamp file to be read.
+        filename (string): Path to bamp file to be read.
 
     Returns:
-    List of numpy.complexes representing the complex amplitudes of the
-    wave represented by the file.
+        List of numpy.complexes representing the complex amplitudes of the
+            wave represented by the file.
     """
-    temp1 = numpy.fromfile(file=filename,dtype=numpy.dtype('f8'))
-    temp2 = temp1.reshape((2,-1),order='F')
+    temp1 = numpy.fromfile(file=filename, dtype='f8')
+    temp2 = temp1.reshape((2, -1), order='F')
     temp3 = []
     for lineNumber in range(temp2.shape[1]):
-        temp3.append(numpy.complex(
-            temp2[0, lineNumber],
-            temp2[1, lineNumber]
-        ))
+        temp3.append(
+            numpy.complex(
+                temp2[0, lineNumber],
+                temp2[1, lineNumber]
+            )
+        )
 
     return temp3
