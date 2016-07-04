8f21ed9431dc14037faaf180bbc1533ce47c927e
Mark Jones <maj@jlab.org>
2016-07-03 20:59:06 -0400

    Cleaned up code to make it easier to read.

--- PyPWA/unoptimized/pythonPWA/model/magV.py
+++ PyPWA/unoptimized/pythonPWA/model/magV.py
@@ -1,18 +1,55 @@
-"""
-.. module:: pythonPWA.model
-   :platform: Unix, Windows, OSX
-   :synopsis: Module describing the various mathematical constructs commonly used in partial wave analysis.
+#    PyPWA, a scientific analysis toolkit.
+#    Copyright (C) 2016  JLab
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-.. moduleauthor:: Brandon Kaleiokalani DeMello <bdemello@jlab.org>
-
-
-"""
 import numpy
-from PyPWA.unoptimized.pythonPWA.utilities.breitWigner import breitWigner
 
-def magV(mass,resonance,wave,waves,normint):
+from PyPWA.unoptimized.pythonPWA.utilities.breitWigner import \
+    breit_wigner_function
+from PyPWA import LICENSE, STATUS, VERSION
+
+__author__ = ["Brandon Kaleiokalani DeMello", "Mark Jones"]
+__credits__ = ["Brandon Kaleiokalani DeMello", "Mark Jones"]
+__email__ = "maj@jlab.org"
+__maintainer__ = "Mark Jones"
+__license__ = LICENSE
+__status__ = STATUS
+__version__ = VERSION
+
+
+def magnitude_v(mass, resonance, wave, waves, normalized_integral):
     """
-    Returns the magnitude of the production amplitued V, for a specified wave, 
-    resonance, wave in the set of waves, and normalization integral.
+    Returns the magnitude of the production amplitude V, for a specified
+    wave, resonance, wave in the set of waves, and normalization integral.
     """
-    return numpy.sqrt((1./normint[wave.epsilon,wave.epsilon,waves.index(wave),waves.index(wave)])*resonance.wR[waves.index(wave)]*resonance.cR*breitWigner(mass,resonance.w0,resonance.r0)*numpy.conjugate(breitWigner(mass,resonance.w0,resonance.r0)))
+
+    breit_wigner_solution = breit_wigner_function(
+        mass, resonance.w0, resonance.r0
+    )
+
+    integral_solution = 1. / normalized_integral[
+        wave.epsilon,
+        wave.epsilon,
+        waves.index(wave),
+        waves.index(wave)
+    ]
+
+    final_solution = numpy.sqrt(
+        integral_solution * resonance.wR[waves.index(wave)] *
+        resonance.cR * breit_wigner_solution *
+        numpy.conjugate(breit_wigner_solution)
+    )
+
+    return final_solution
