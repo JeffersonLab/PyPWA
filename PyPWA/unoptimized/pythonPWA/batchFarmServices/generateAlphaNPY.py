ecd8cef20f00a60b3d903a153ddd59d72a06d769
Mark Jones <maj@jlab.org>
2016-07-04 00:53:45 -0400

    Factored out some dependencies.

--- PyPWA/unoptimized/pythonPWA/batchFarmServices/generateAlphaNPY.py
+++ PyPWA/unoptimized/pythonPWA/batchFarmServices/generateAlphaNPY.py
@@ -6,42 +6,40 @@
 .. moduleauthor:: Stephanie K. A. Bramlett <skab@jlab.org>, Joshua Pond <jpond@jlab.org>
 
 
-""" 
-import os, sys, numpy
-from PyPWA.unoptimized.pythonPWA.utilities.ThreeVec import ThreeVector
-from PyPWA.unoptimized.pythonPWA.utilities.FourVec import FourVector
-from PyPWA.unoptimized.pythonPWA.fileHandlers.gampTranslator import \
-    GampTranslator
-import math
+"""
+import os, sys
+
+import numpy
+
+from PyPWA.unoptimized.pythonPWA.utilities import vectors
+from PyPWA.libs.data.builtin import gamp
 
 class generateAlphas(object):
     """
-        This program calculates the value of alpha for every event in a .gamp file and saves it to a .txt list, one event per line. 
+        This program calculates the value of alpha for every event in a .gamp file and saves it to a .txt list, one event per line.
     """
-    
+
     alphaName = "alpha"
     labelOut = ""
-    
+
     def __init__(self, mode, indir, gfile):
         """
             Default generatAlphas constructor
 
             Kwargs:
-            mode (int):  8 = p pi+ pi- pi0, 22 = p p+ p-, 24 = p k+ k-, 42 = gamma p --> p Ks Ks (pi+ pi- pi+ pi-) 
+            mode (int):  8 = p pi+ pi- pi0, 22 = p p+ p-, 24 = p k+ k-, 42 = gamma p --> p Ks Ks (pi+ pi- pi+ pi-)
             indir (string): Full file path to directory of file
-            gfile (string): File name without .gamp extension. 
+            gfile (string): File name without .gamp extension.
         """
-        
+
         self.mode = mode
         self.indir = indir
         self.gfile = gfile+".gamp"
         self.nfile = gfile+".npy"
         f = gfile.partition(".")[0]
-        self.gampT = GampTranslator(os.path.join(self.indir, self.gfile))
-        if not os.path.isfile(os.path.join(self.indir,self.nfile)):
-            self.gampT.translate(os.path.join(self.indir,self.nfile))
-        self.gampList=numpy.load(os.path.join(self.indir,self.nfile))
-        self.alphaName = "alpha" + f + ".txt"        
+        self.gampT = gamp.GampMemory()
+        self.gampList= self.gampT.parse(os.path.join(self.indir, self.gfile))
+        self.alphaName = "alpha" + f + ".txt"
         self.alphalist = []
         if self.mode == "8":
             self.analyze8()
@@ -53,7 +51,7 @@ class generateAlphas(object):
             self.analyze42()
         if self.mode == "EtaPiN":
             self.analyzeEtaPiN
-    
+
     def analyze8(self):
         '''
             p pi+ pi- pi0
@@ -62,42 +60,51 @@ class generateAlphas(object):
             event = self.gampT.write_event(self.gampList[i, :, :])
             for particles in event.particles:
                 if particles.particleID == 14.0: #proton
-                    p = FourVector(float(particles.particleE), 
-                                    float(particles.particleXMomentum),
-                                    float(particles.particleYMomentum),
-                                    float(particles.particleZMomentum))
+                    p = vectors.FourVector(
+                        float(particles.particleE),
+                        float(particles.particleXMomentum),
+                        float(particles.particleYMomentum),
+                        float(particles.particleZMomentum)
+                    )
                 if particles.particleID == 1.0: #photon gamma
-                    bm = FourVector(float(particles.particleE), 
-                                    float(particles.particleXMomentum),
-                                    float(particles.particleYMomentum),
-                                    float(particles.particleZMomentum))
+                    bm = vectors.FourVector(
+                        float(particles.particleE),
+                        float(particles.particleXMomentum),
+                        float(particles.particleYMomentum),
+                        float(particles.particleZMomentum)
+                    )
+
                 if particles.particleID == 9.0: #p-
-                    pim = FourVector(float(particles.particleE), 
-                                    float(particles.particleXMomentum),
-                                    float(particles.particleYMomentum),
-                                    float(particles.particleZMomentum))
+                    pim = vectors.FourVector(
+                        float(particles.particleE),
+                        float(particles.particleXMomentum),
+                        float(particles.particleYMomentum),
+                        float(particles.particleZMomentum)
+                    )
                 if particles.particleID == 8.0: #p+
-                    pip = FourVector(float(particles.particleE), 
-                                    float(particles.particleXMomentum),
-                                    float(particles.particleYMomentum),
-                                    float(particles.particleZMomentum))
-            
-            ptarget = FourVector(.938, 0.,0.,0.)  
+                    pip = vectors.FourVector(
+                        float(particles.particleE),
+                        float(particles.particleXMomentum),
+                        float(particles.particleYMomentum),
+                        float(particles.particleZMomentum)
+                    )
+
+            ptarget = vectors.FourVector(.938, 0.,0.,0.)
             initp = bm + ptarget
             finalp = pip + pim + p
             pi0 = initp + finalp
             pmeson = pip + pim + pi0
             pMeson = pmeson.p
             beam3 = bm.p
-            polarization = ThreeVector(0.,1.,0.)
+            polarization = vectors.ThreeVector(0.,1.,0.)
             Normal = beam3 * pMeson
-            
-            cosAlpha = Normal.dot(polarization) / Normal.r()
-            alpha = math.acos(cosAlpha)
+
+            cosAlpha = Normal.dot(polarization) / Normal.length()
+            alpha = numpy.arccos(cosAlpha)
             sys.stdout.write(str(i)+"\r")
             sys.stdout.flush()
             self.alphalist.append(alpha)
-    
+
     def analyze22(self):
         '''
             p p+ p-
@@ -106,22 +113,22 @@ class generateAlphas(object):
             event = self.gampT.write_event(self.gampList[i, :, :])
             for particles in event.particles:
                 if particles.particleID == 14.0: #proton
-                    pp = FourVector(float(particles.particleE), 
+                    pp = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 1.0: #photon gamma
-                    bm = FourVector(float(particles.particleE), 
+                    bm = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 9.0: #p-
-                    pim = FourVector(float(particles.particleE), 
+                    pim = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 8.0: #p+
-                    pip = FourVector(float(particles.particleE), 
+                    pip = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
@@ -129,38 +136,38 @@ class generateAlphas(object):
             pmeson = pip + pim
             pMeson = pmeson.p
             beam3 = bm.p
-            polarization = ThreeVector(0.,1.,0.)
+            polarization = vectors.ThreeVector(0.,1.,0.)
             Normal = beam3 * pMeson
-            
-            cosAlpha = Normal.dot(polarization) / Normal.r()
-            alpha = math.acos(cosAlpha)
+
+            cosAlpha = Normal.dot(polarization) / Normal.length()
+            alpha = numpy.arccos(cosAlpha)
             self.alphalist.append(alpha)
-            
-    
+
+
     def analyze24(self):
         """
-            p k+ k-  
+            p k+ k-
         """
         for i in range(int(self.gampList.shape[0])):
             event = self.gampT.write_event(self.gampList[i, :, :])
             for particles in event.particles:
                 if particles.particleID == 14.0: #proton
-                    pp = FourVector(float(particles.particleE), 
+                    pp = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 1.0: #photon gamma
-                    bm = FourVector(float(particles.particleE), 
+                    bm = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 12.0: #K-
-                    km = FourVector(float(particles.particleE), 
+                    km = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 11.0: #K+
-                    kp = FourVector(float(particles.particleE), 
+                    kp = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
@@ -173,14 +180,14 @@ class generateAlphas(object):
                 #determine alpha
             pMeson = pmeson.p
             beam3 = bm.p
-            polarization = ThreeVector(0.,1.,0.)
+            polarization = vectors.ThreeVector(0.,1.,0.)
             Normal = beam3 * pMeson
 
-            cosAlpha = Normal.dot(polarization) / Normal.r()
-            alpha = math.acos(cosAlpha)
+            cosAlpha = Normal.dot(polarization) / Normal.length()
+            alpha = numpy.arccos(cosAlpha)
             #print alpha
             self.alphalist.append(alpha)
-    
+
     def analyze42(self):
         """
             gamma p --> p Ks Ks (pi+ pi- pi+ pi-)
@@ -190,87 +197,86 @@ class generateAlphas(object):
             nks = 0
             for particles in event.particles:
                 if particles.particleID == 14.0: #proton
-                    p = FourVector(float(particles.particleE), 
+                    p = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 1.0: #photon gamma
-                    bm = FourVector(float(particles.particleE), 
+                    bm = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 16.0: #Ks
                     if nks == 0:
-                        ks1 = FourVector(float(particles.particleE), 
+                        ks1 = vectors.FourVector(float(particles.particleE),
                                         float(particles.particleXMomentum),
                                         float(particles.particleYMomentum),
                                         float(particles.particleZMomentum))
-                    else: 
-                        ks2 = FourVector(float(particles.particleE), 
+                    else:
+                        ks2 = vectors.FourVector(float(particles.particleE),
                                         float(particles.particleXMomentum),
                                         float(particles.particleYMomentum),
                                         float(particles.particleZMomentum))
                     nks += 1
-                    
+
             pmeson = ks1 + ks2
             pMeson = pmeson.p
             beam3 = bm.p
-            polarization = ThreeVector(0.,1.,0.)
+            polarization = vectors.ThreeVector(0.,1.,0.)
             Normal = beam3 * pMeson
 
-            cosAlpha = Normal.dot(polarization) / Normal.r()
-            alpha = math.acos(cosAlpha)
+            cosAlpha = Normal.dot(polarization) / Normal.length()
+            alpha = numpy.arccos(cosAlpha)
 
             self.alphalist.append(alpha)
-            
-        
+
+
     def analyzeEtaPiN(self):
         for i in range(int(self.gampList.shape[0])):
             event = self.gampT.write_event(self.gampList[i, :, :])
             for particles in event.particles:
                 if particles.particleID == 13.0: #neutron
-                    n = FourVector(float(particles.particleE), 
+                    n = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 1.0: #photon gamma
-                    bm = FourVector(float(particles.particleE), 
+                    bm = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 12.0: #eta
-                    eta = FourVector(float(particles.particleE), 
+                    eta = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
                 if particles.particleID == 11.0: #p+
-                    pip = FourVector(float(particles.particleE), 
+                    pip = vectors.FourVector(float(particles.particleE),
                                     float(particles.particleXMomentum),
                                     float(particles.particleYMomentum),
                                     float(particles.particleZMomentum))
             pmeson = eta + pip
             pMeson = pmeson.p
             beam3 = bm.p
-            polarization = ThreeVector(0.,1.,0.)
+            polarization = vectors.ThreeVector(0.,1.,0.)
             Normal = beam3 * pMeson
 
-            cosAlpha = Normal.dot(polarization) / Normal.r()
-            alpha = math.acos(cosAlpha)
+            cosAlpha = Normal.dot(polarization) / Normal.length()
+            alpha = numpy.arccos(cosAlpha)
 
             self.alphalist.append(alpha)
-            
-        
+
     def toFile(self):
         """
-            Saves the list to a file. 
+            Saves the list to a file.
         """
         f = open(os.path.join(self.indir, self.alphaName), 'w')
         for alpha in self.alphalist:
             f.write(str(alpha) + "\n")
         f.close()
 
-    
-    
+
+
 indir = sys.argv[2]
 gfile = sys.argv[3]
 mode = sys.argv[1]
