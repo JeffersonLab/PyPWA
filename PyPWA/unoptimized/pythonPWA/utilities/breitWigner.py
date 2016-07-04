1a33847f932a4ccb6dfc7cf983c550339338dc4b
Mark Jones <maj@jlab.org>
2016-07-03 21:09:55 -0400

    Refactored for legibility.

--- PyPWA/unoptimized/pythonPWA/utilities/breitWigner.py
+++ PyPWA/unoptimized/pythonPWA/utilities/breitWigner.py
@@ -1,27 +1,49 @@
-"""
-.. module:: pythonPWA.utilities
-   :platform: Unix, Windows, OSX
-   :synopsis: Module containing various useful scripts.
+#    PyPWA, a scientific analysis toolkit.
+#    Copyright (C) 2016  JLab
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-.. moduleauthor:: Brandon Kaleiokalani DeMello <bdemello@jlab.org>
-
-
-"""
 import numpy
 
-def breitWigner(mass,resMass,gamma):
+from PyPWA import LICENSE, STATUS, VERSION
+
+__author__ = ["Brandon Kaleiokalani DeMello", "Mark Jones"]
+__credits__ = ["Brandon Kaleiokalani DeMello", "Mark Jones"]
+__email__ = "maj@jlab.org"
+__maintainer__ = "Mark Jones"
+__license__ = LICENSE
+__status__ = STATUS
+__version__ = VERSION
+
+
+def breit_wigner_function(mass, resonance_mass, γ):
     """
-    Briet Wigner function for a given mass, resonance mass, and resonance width.  Note that here, it assumes a fixed
-    resonance width.
+    Briet Wigner function for a given mass, resonance mass, and resonance
+    width.  Note that here, it assumes a fixed resonance width.
 
     Args:
-    mass (float):
-    resMass (float):
-    gamma (float): 
+        mass (numpy.float64):
+        resonance_mass (numpy.float64):
+        γ (numpy.float64):
 
     Returns:
-    Numpy.complex value representing the Briet Wigner function value.
+        Numpy.complex value representing the Briet Wigner function value.
     """
-    dr=mass-resMass
-    denom=dr*dr+gamma*gamma/4.
-    return numpy.complex(dr*gamma/(2.*denom),gamma*gamma/(denom*4.))
+    Δ_radius = mass - resonance_mass
+    denominator = Δ_radius * Δ_radius + γ * γ / 4.
+
+    real = Δ_radius * γ / (2. * denominator)
+    imaginary = γ * γ / (denominator * 4.)
+
+    return numpy.complex(real, imaginary)
