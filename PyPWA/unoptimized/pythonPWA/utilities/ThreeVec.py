c9d01a7f350d74e985c6ccdd8cbd6f9774a6c2b8
Mark Jones <maj@jlab.org>
2016-07-03 23:28:26 -0400

    Refactored to be more legible.

--- PyPWA/unoptimized/pythonPWA/utilities/ThreeVec.py
+++ PyPWA/unoptimized/pythonPWA/utilities/ThreeVec.py
@@ -1,116 +1,97 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 26 13:48:01 2014
-
-@author: sbramlett
-"""
-
-import math
-class ThreeVector(object):
-    vec = []
-
-    def __init__(self, x = 0, y = 0, z = 0):
-        if type(x) is not list:
-            self.x = x
-            self.y = y
-            self.z = z
-            self.vec = [x,y,z]
-        if type(x) is list and len(x) == 3:
-            temp = x
-            self.x = temp[0]
-            self.y = temp[1]
-            self.z = temp[2]
-            self.vec = [self.x, self.y, self.z]
-    '''
-    For printing the Three Vector
-    '''
-    def __repr__(self):
-        return str(self.vec)
-    '''
-    Add two Three Vectors together
-    '''  
-    def __add__(self, n):
-        px = self.x + n.x
-        py = self.y + n.y
-        pz = self.z + n.z
-        pvec = ThreeVector(px, py, pz)
-        return pvec
-    '''
-    Subtract two Three Vectors together
-    '''  
-    def __sub__(self, n):
-        px = self.x - n.x
-        py = self.y - n.y
-        pz = self.z - n.z
-        pvec = ThreeVector(px, py, pz)
-        return pvec
-    '''    
-    def __mul__(self, n):
-        px = self.x * n.x
-        py = self.y * n.y
-        pz = self.z * n.z
-        pvec = ThreeVector(px, py, pz)
-        return pvec
-    '''
-    '''
-    Cross or scalar product
-    '''
-    def __mul__(self, n):
-       if type(n) is ThreeVector:
-            px = self.y * n.z - self.z * n.y
-            py = self.z * n.x - self.x * n.z
-            pz = self.x * n.y - self.y * n.x
-            pvec = ThreeVector(px, py, pz)
-            return pvec
-       else:
-            print( n)
-            px = self.x * n
-            py = self.y * n
-            pz = self.z * n
-            pvec = ThreeVector(px, py, pz)
-            return pvec               
-    '''
-    #Dot Product of two Three Vectors together
-    '''  
-    def dot(self, A):
-        new = self.x * A.x + self.y * A.y + self.z * A.z
-        return new
-    '''
-    #The Length of the vector
-    '''
-    def r(self):
-        r = (self.x**2 + self.y**2 + self.z**2)**(1./2.)
-        return r
-    '''
-    #The length of the vectore squared
-    '''
-    def lenSq(self):
-        return self.r()**2
-    '''
-    '''
-    def cosTheta(self, args):
-        if type(args) is ThreeVector:
-            n = self.dot(args)
-            ret = n / self.r() / args.r()
-            return ret
-    def CosTheta(self):
-        return self.z /self.r()
-           
-    def sinTheta(self):
-        return (self.x**2 + self.y**2)/r()
-    def phi(self):
-        return math.atan2(self.y, self.x)
-    def theta(self):
-        return math.acos(self.CosTheta())
-   
-#x = [1, 2, 3]
-#y = [4, 5, 6]
-#print np.cross(x, y)
+#    PyPWA, a scientific analysis toolkit.
+#    Copyright (C) 2016  JLab
 #
-#P1 = ThreeVector(x)
-#print str(P1.lenSq())
-#print P1
-#P2 = ThreeVector(y)
-#print P1 * P2
-#print np.cross(P1.vec, P2.vec)
-        
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy
+
+from PyPWA import LICENSE, STATUS, VERSION
+
+__author__ = ["Stephanie Bramlett", "Mark Jones"]
+__credits__ = ["Stephanie Bramlett", "Mark Jones"]
+__email__ = "maj@jlab.org"
+__maintainer__ = "Mark Jones"
+__license__ = LICENSE
+__status__ = STATUS
+__version__ = VERSION
+
+
+class ThreeVector(object):
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.vector = [self.x, self.y, self.z]
+
+    def __repr__(self):
+        return str(self.vector)
+
+    def __add__(self, vector):
+        px = self.x + vector.x
+        py = self.y + vector.y
+        pz = self.z + vector.z
+        return ThreeVector(px, py, pz)
+
+    def __sub__(self, vector):
+        px = self.x - vector.x
+        py = self.y - vector.y
+        pz = self.z - vector.z
+        return ThreeVector(px, py, pz)
+
+    def __mul__(self, vector):
+        if isinstance(vector, ThreeVector):
+            px = self.y * vector.z - self.z * vector.y
+            py = self.z * vector.x - self.x * vector.z
+            pz = self.x * vector.y - self.y * vector.x
+        else:
+            px = self.x * vector
+            py = self.y * vector
+            pz = self.z * vector
+        return ThreeVector(px, py, pz)
+
+    def dot(self, three_vec):
+        new_x = self.x * three_vec.x
+        new_y = self.y * three_vec.y
+        new_z = self.z * three_vec.z
+        return new_x + new_y + new_z
+
+    def cosTheta(self, three_vec):
+        if isinstance(three_vec, ThreeVector):
+            dot_product = self.dot(three_vec)
+            return dot_product / self.length / three_vec.length
+
+    @property
+    def length(self):
+        return numpy.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    @property
+    def length_square(self):
+        return self.length ** 2
+
+    @property
+    def cos_theta(self):
+        return self.z / self.length
+
+    @property
+    def sin_theta(self):
+        return (self.x**2 + self.y**2) / self.length
+
+    @property
+    def phi(self):
+        return numpy.arctan2(self.y, self.x)
+
+    @property
+    def theta(self):
+        return numpy.arccos(self.cos_theta)
