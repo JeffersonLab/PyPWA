4815aa949a3b2a68fcd205cb475b05571d955cc8
Mark Jones <maj@jlab.org>
2016-07-04 00:01:42 -0400

    Minor refactoring.

--- PyPWA/unoptimized/pythonPWA/model/nExpected.py
+++ PyPWA/unoptimized/pythonPWA/model/nExpected.py
@@ -1,13 +1,18 @@
 import numpy
 
 
-def gampLen(path):
+# TODO: Fix the memory issues with this file.
+# TODO: Extract the file logic from this file.
+# TODO: Move all these functions into a single object.
+
+def gamp_length(path):
     ins = open(path)
     for line in ins:
-        leng = line.strip("\n")
-    return float(leng)
+        length = line.strip("\n")
+    return float(length)
 
-def countAlphas(path):
+
+def count_alphas(path):
         """
         Returns the length of an alpha angle file.
 
@@ -17,37 +22,58 @@ def countAlphas(path):
         Returns:
         Int equivalent to the length of the alpha file.
         """
-        Alpha = open(path,'r')
+
+        Alpha = open(path, 'length')
         AlphaList = Alpha.readlines()
-        
-        return float(len(AlphaList))  
-        
-def etaX(apath,rpath):
+        return float(len(AlphaList))
+
+
+def etaX(apath, rpath):
     """
     Calculates the acceptance.
 
     Returns:
     Float value of the acceptance.
     """
-    #etax=(gampLen(apath)/gampLen(rpath))
-    etaX=(countAlphas(apath)/countAlphas(rpath))
-    return etaX 
+    etaX = (count_alphas(apath) / count_alphas(rpath))
+    return etaX
 
-def nExpForFixedV1V2(vList,waves,normint,apath,rpath):
+
+def nExpForFixedV1V2(vList, waves, normint, apath, rpath):
     """
     calculates the number of _events for fitted v1 and v2 values.
     """
     ret=0.
     for wave1 in waves:
         for wave2 in waves:
-            psi=normint[wave1.epsilon,wave2.epsilon,waves.index(wave1),waves.index(wave2)]
-            
-            ret+=vList[waves.index(wave1)]*numpy.conjugate(vList[waves.index(wave2)])*psi
-    return etaX(apath,rpath)*ret.real
-    
-def nExpForFixedV1V2AndWave(v,waves,wave,normint,apath,rpath):
+            psi = normint[
+                wave1.epsilon,
+                wave2.epsilon,
+                waves.index(wave1),
+                waves.index(wave2)
+            ]
+
+            ret += vList[waves.index(wave1)] \
+                   * numpy.conjugate(vList[waves.index(wave2)]) \
+                   * psi
+
+    return etaX(apath, rpath) * ret.real
+
+
+def nExpForFixedV1V2AndWave(v, waves, wave, normint, apath, rpath):
     """
-    calculates the number of _events for fitted v1 and v2 values for a specific
-    wave.
+    calculates the number of _events for fitted v1 and v2 values for a
+    specific wave.
     """
-    return etaX(apath,rpath)*v*numpy.conjugate(v)*normint[wave.epsilon,wave.epsilon,waves.index(wave),waves.index(wave)]
+    extracted_integral = normint[
+        wave.epsilon,
+        wave.epsilon,
+        waves.index(wave),
+        waves.index(wave)
+    ]
+
+    acceptance = etaX(apath, rpath)
+
+    final_value = acceptance * v * numpy.conjugate(v) * extracted_integral
+
+    return final_value
