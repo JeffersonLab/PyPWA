127d141c159d907ca67c6b1288380432fb3c8490
Mark Jones <maj@jlab.org>
2016-07-03 23:59:49 -0400

    Simple refactoring for legibility.

--- PyPWA/unoptimized/pythonPWA/utilities/dataSimulator.py
+++ PyPWA/unoptimized/pythonPWA/utilities/dataSimulator.py
@@ -9,24 +9,21 @@
 """
 from random import random
 
-from PyPWA.unoptimized.pythonPWA.fileHandlers.gampReader import gampReader
-from PyPWA.unoptimized.pythonPWA.model.intensity import intensity
+from PyPWA.libs.data.builtin import gamp
+from PyPWA.unoptimized.pythonPWA.model.intensity import Intensity
 
 
 class dataSimulator(object):
     """
-    This is the main simulator class. By instantiating the variables to the desired values
-    you can easily simulate data with any characteristics that you would like.
+    This is the main simulator class. By instantiating the variables to
+    the desired values you can easily simulate data with any
+    characteristics that you would like.
     """
-    def __init__(self,
-                 mass=1010.,
-                 waves=[],
-                 resonances=[],
-                 normint=None,
-                 productionAmplitudes=[],
-                 alphaList=[],
-                 dataDir=None,
-                 beamPolarization=.4):
+    def __init__(
+            self, mass=1010., waves=[],
+            resonances=[], normint=None,
+            productionAmplitudes=[],
+            alphaList=[], beamPolarization=.4):
         """
         Default constructor for the dataSimulator class.
 
@@ -34,27 +31,26 @@ class dataSimulator(object):
         mass (float):
         waves (list):
         resonances (list):
-        normint (pythonPWA.model.normInt):
+        normint (pythonPWA.model.NormalizeIntegral):
         productionAmplitudes (list):
         alphaList (list):
         beamPolarization (float):
 
         """
 
-        
-        self.mass=mass
-        self.waves=waves
-        self.resonances=resonances
-        self.normint=normint
-        self.productionAmplitudes=productionAmplitudes
-        self.alphaList=alphaList
-        self.beamPolarization=beamPolarization
-        self.intensity=intensity(resonances=self.resonances,
-                                 waves=self.waves,
-                                 productionAmplitudes=self.productionAmplitudes,
-                                 normint=self.normint,
-                                 alphaList=self.alphaList,
-                                 beamPolarization=self.beamPolarization)
+        self.mass = mass
+        self.waves = waves
+        self.resonances = resonances
+        self.normint = normint
+        self.productionAmplitudes = productionAmplitudes
+        self.alphaList = alphaList
+        self.beamPolarization = beamPolarization
+        self.intensity = Intensity(
+            resonances=self.resonances, waves=self.waves,
+            productionAmplitudes=self.productionAmplitudes,
+            normint=self.normint, alphaList=self.alphaList,
+            beamPolarization=self.beamPolarization
+        )
 
     def calcIList(self):
         """
@@ -65,13 +61,14 @@ class dataSimulator(object):
         """
         iList=[]
         for event in range(len(self.alphaList)):
-            i=self.intensity.calculate(self.mass,event)
+            i=self.intensity.calculate(self.mass , event)
             iList.append(i)
         return iList
-    
-    def calcWList(self,iList,iMax,inputGampFile,outputRawGampFile,outputAccGampFile,inputPfFile):
+
+    def calcWList(self, iList, iMax, inputGampFile, outputRawGampFile, outputAccGampFile, inputPfFile):
         """
-        Calculates the list of weights for each event, and writes the raw gamp file.
+        Calculates the list of weights for each event, and writes the raw
+        gamp file.
 
         Args:
         iList (list):
@@ -82,19 +79,19 @@ class dataSimulator(object):
         inputPfFile (file):
 
         """
-        wList=[x/iMax for x in iList]
-        igreader=gampReader(gampFile=inputGampFile)
-        inputGampEvents=igreader.readGamp()
+        wList = [x / iMax for x in iList]
+        igreader = gampReader(gampFile=inputGampFile)
+        inputGampEvents = igreader.readGamp()
 
-        rawGampEvents=[]
+        rawGampEvents = []
         for wn in range(len(wList)):
-            if wList[wn]>random():
-                inputGampEvents[wn].raw=True
+            if wList[wn] > random():
+                inputGampEvents[wn].raw = True
                 rawGampEvents.append(inputGampEvents[wn])
-        
+
         for rawGamps in rawGampEvents:
-            rawGamps.writeGamp(outputRawGampFile)        
-        
+            rawGamps.writeGamp(outputRawGampFile)
+
         outputRawGampFile.close()
         outputAccGampFile.close()
-    
+
