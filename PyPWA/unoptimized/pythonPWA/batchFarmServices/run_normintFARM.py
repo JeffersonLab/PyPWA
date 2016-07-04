78355766f1e061939d17bf444d05ce0a8c98d0c5
Mark Jones <maj@jlab.org>
2016-07-04 00:55:17 -0400

    Removed trailing spaces.

--- PyPWA/unoptimized/pythonPWA/batchFarmServices/run_normintFARM.py
+++ PyPWA/unoptimized/pythonPWA/batchFarmServices/run_normintFARM.py
@@ -6,19 +6,19 @@
 .. moduleauthor:: Joshua Pond <jpond@jlab.org>
 
 
-""" 
+"""
 import os
-import sys 
+import sys
 sys.path.append(os.path.join(sys.argv[4],"pythonPWA"))
 import numpy
 from PyPWA.unoptimized.pythonPWA.fileHandlers.getWavesGen import getwaves
-from PyPWA.unoptimized.pythonPWA.model.normInt import normInt
+from PyPWA.unoptimized.pythonPWA.model.normInt import normalize_integral
 """
-    This is the program that does the work of calculating the normilization integral and returning it as a .npy file. 
+This is the program that does the work of calculating the normilization integral and returning it as a .npy file.
 """
 dataDir=sys.argv[1]
-alphaList=numpy.loadtxt(os.path.join(dataDir,sys.argv[2]))    
+alphaList=numpy.loadtxt(os.path.join(dataDir,sys.argv[2]))
 waves=getwaves(dataDir)
-rInt=normInt(waves=waves,alphaList=alphaList,beamPolarization=sys.argv[3])
+rInt=normalize_integral(waves=waves, alpha_list=alphaList, beam_polarization=sys.argv[3])
 rInt.execute()
 rInt.save(dataDir)
