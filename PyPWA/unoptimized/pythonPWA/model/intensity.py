5972c34b43d6d8ee6b2585ac6a0d5cf4ec850c77
Mark Jones <maj@jlab.org>
2016-07-03 23:54:45 -0400

    Updated to use refactored objects.

--- PyPWA/unoptimized/pythonPWA/model/intensity.py
+++ PyPWA/unoptimized/pythonPWA/model/intensity.py
@@ -8,46 +8,45 @@
 
 """
 import numpy
-from PyPWA.unoptimized.pythonPWA.model.complexV import complexV
-from PyPWA.unoptimized.pythonPWA.model.spinDensity import spinDensity
+from PyPWA.unoptimized.pythonPWA.model import models
 
-class intensity(object):
-    """
-    This represents the intensity function used in the PWA model.
-    """
-    def __init__(self,
-                 resonances=[],
-                 waves=[],
-                 productionAmplitudes=[],
-                 normint=None,
-                 alphaList=[],
-                 beamPolarization=.6
-                 ):
+
+class Intensity(object):
+    def __init__(
+            self,
+            resonances=[],
+            waves=[],
+            productionAmplitudes=[],
+            normint=None,
+            alphaList=[],
+            beamPolarization=.6
+    ):
         """
-        Default constructor for the intensity class.
+        This represents the intensity function used in the PWA model.
 
-        Kwargs:
-        resonances (list):
-        waves (list):
-        productionAmplitudes (list):
-        normint (pythonPWA.model.normInt):
-        alphaList (list):
-        beamPolarization (float):
+        Args:
+            resonances (list):
+            waves (list):
+            productionAmplitudes (list):
+            normint (pythonPWA.model.NormalizeIntegral):
+            alphaList (list):
+            beamPolarization (float):
 
         """
-        self.resonances=resonances
-        self.waves=waves
-        self.productionAmplitudes=productionAmplitudes
-        self.normint=normint
-        self.alphaList=alphaList
-        self.beamPolarization=beamPolarization
-        #DONT FORGET TO FIX AFTER DEBUGGING        
-        self.testWriting=0
-        self.testValues=[]
+        self.resonances = resonances
+        self.waves = waves
+        self.productionAmplitudes = productionAmplitudes
+        self.normint = normint
+        self.alphaList = alphaList
+        self.beamPolarization = beamPolarization
+        #DONT FORGET TO FIX AFTER DEBUGGING
+        self.testWriting = 0
+        self.testValues = []
 
-    def calculate(self,mass,eventNumber):
+    def calculate(self, mass, eventNumber):
         """
-        Calculate function for the instensity class, returns the value for the instensity for specified mass and event number.
+        Calculate function for the intensity class, returns the value for
+        the intensity for specified mass and event number.
 
         Args:
         mass (float):
@@ -60,29 +59,27 @@ class intensity(object):
         ret=numpy.complex(0.,0.)
         self.testValues=[]
         for resonance1 in self.resonances:
-            #print"resonance1=",resonance1.r0
             for resonance2 in self.resonances:
-                #print"resonance2=",resonance2.r0
                 for wave1 in self.waves:
                     for wave2 in self.waves:
-                        if len(self.productionAmplitudes)==0:
-                            ret+=complexV(resonance1,wave1,self.waves,self.normint,mass)*numpy.conjugate(complexV(resonance2,wave2,self.waves,self.normint,mass))*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*spinDensity(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon]
+                        if len(self.productionAmplitudes) == 0:
+                            ret += models.complexV(resonance1,wave1,self.waves,self.normint,mass)*numpy.conjugate(models.complexV(resonance2,wave2,self.waves,self.normint,mass))*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*models.SpinDensity.calculate(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon]
                         if len(self.productionAmplitudes)!=0:
-                            ret+=self.productionAmplitudes[self.waves.index(wave1)]*numpy.conjugate(self.productionAmplitudes[self.waves.index(wave2)])*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*spinDensity(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon]
+                            ret+=self.productionAmplitudes[self.waves.index(wave1)]*numpy.conjugate(self.productionAmplitudes[self.waves.index(wave2)])*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*models.SpinDensity.calculate(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon]
                             if self.testWriting==1:
-                                if [self.waves.index(wave1),self.waves.index(wave2),self.resonances.index(resonance1),self.resonances.index(resonance2),complexV(resonance1,wave1,self.waves,self.normint,mass),complexV(resonance2,wave2,self.waves,self.normint,mass),complexV(resonance1,wave1,self.waves,self.normint,mass)*numpy.conjugate(complexV(resonance2,wave2,self.waves,self.normint,mass))*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*spinDensity(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon],wave1.complexamplitudes[eventNumber],wave2.complexamplitudes[eventNumber]] not in self.testValues:
-                                   self.testValues.append([self.waves.index(wave1),self.waves.index(wave2),self.resonances.index(resonance1),self.resonances.index(resonance2),complexV(resonance1,wave1,self.waves,self.normint,mass),complexV(resonance2,wave2,self.waves,self.normint,mass),complexV(resonance1,wave1,self.waves,self.normint,mass)*numpy.conjugate(complexV(resonance2,wave2,self.waves,self.normint,mass))*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*spinDensity(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon],wave1.complexamplitudes[eventNumber],wave2.complexamplitudes[eventNumber]]) 
+                                if [self.waves.index(wave1),self.waves.index(wave2),self.resonances.index(resonance1),self.resonances.index(resonance2),models.complexV(resonance1,wave1,self.waves,self.normint,mass),models.complexV(resonance2,wave2,self.waves,self.normint,mass),models.complexV(resonance1,wave1,self.waves,self.normint,mass)*numpy.conjugate(models.complexV(resonance2,wave2,self.waves,self.normint,mass))*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*models.SpinDensity.calculate(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon],wave1.complexamplitudes[eventNumber],wave2.complexamplitudes[eventNumber]] not in self.testValues:
+                                   self.testValues.append([self.waves.index(wave1),self.waves.index(wave2),self.resonances.index(resonance1),self.resonances.index(resonance2),models.complexV(resonance1,wave1,self.waves,self.normint,mass),models.complexV(resonance2,wave2,self.waves,self.normint,mass),models.complexV(resonance1,wave1,self.waves,self.normint,mass)*numpy.conjugate(models.complexV(resonance2,wave2,self.waves,self.normint,mass))*wave1.complexamplitudes[eventNumber]*numpy.conjugate(wave2.complexamplitudes[eventNumber])*models.SpinDensity.calculate(self.beamPolarization,self.alphaList[eventNumber])[wave1.epsilon,wave2.epsilon],wave1.complexamplitudes[eventNumber],wave2.complexamplitudes[eventNumber]])
         if self.testWriting==1:
             for items in self.testValues:
                 print("="*10)
                 print("wave1",items[0])
-                
+
                 print("wave2",items[1])
                 print("resonance1:",items[2])
                 print("resonance2:",items[3])
                 print("v1:",items[4])
                 print("v2:",items[5])
-                
+
                 print("amplitude1:",items[7])
                 print("amplitude2:",items[8])
                 print("term:",items[6])
