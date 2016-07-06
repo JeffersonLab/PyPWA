f274f3e002787677fcf1502ef948c29706db3642
Mark Jones <maj@jlab.org>
2016-07-06 12:41:23 -0400

    Slight refactoring to be more readable.

--- micAMP/fitterMIC.py
+++ micAMP/fitterMIC.py
@@ -2,14 +2,15 @@
 import numpy
 import PyPWA.proc.calculation_tools
 import ctypes
-from ctypes import *
-the_lib = ctypes.cdll.LoadLibrary("/home/mvick/PyPWA/Xeon Phi/libIM.so")
+import os
+
+the_lib = ctypes.cdll.LoadLibrary(os.getcwd() + "libIM.so")
 
 the_lib.argtypes = ctypes.c_double
 the_lib.LogLike_.restype = ctypes.c_double
 
 set_up = .5
-initial_settings = { "p": -9.0 }
+initial_settings = {"p": -9.0}
 parameters = ["p"]
 strategy = 0
 ncall = 1000
@@ -17,18 +18,22 @@ ncall = 1000
 the_lib.Initialize()
 
 
-def likelihood_Wrapper(parameter):
-	something = -numpy.float64(the_lib.LogLike_(ctypes.c_double(parameter)))
-	print ("parameter = " + str(parameter) + " loglike = " + str(something))
-	return something
+def likelihood_wrapper(parameter):
+    likelihood = -numpy.float64(
+        the_lib.LogLike_(ctypes.c_double(parameter))
+    )
 
-minimization = PyPWA.proc.calculation_tools.Minimizer(likelihood_Wrapper,
-												 parameters, initial_settings, 
-												 strategy, set_up, ncall)
-												 
+    print("parameter = {0} loglike = {1}".format(
+        str(parameter), str(likelihood)
+    ))
+
+    return likelihood
+
+minimization = PyPWA.proc.calculation_tools.Minimizer(
+    likelihood_wrapper, parameters, initial_settings,
+    strategy, set_up, ncall
+)
 
 minimization.min()
 
 print(minimization.covariance)
-#for i in range(60):
-#likelihood_Wrapper(i/3.0-10)
