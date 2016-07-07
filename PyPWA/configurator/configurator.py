ec8c64e7bfb693fadd2596265a174c2df372b6d5
Mark Jones <maj@jlab.org>
2016-07-07 11:33:53 -0400

    Added options object and initializer.

--- PyPWA/configurator/configurator.py
+++ PyPWA/configurator/configurator.py
@@ -1,3 +1,4 @@
+
 #    PyPWA, a scientific analysis toolkit.
 #    Copyright (C) 2016  JLab
 #
@@ -23,6 +24,8 @@ needs to be structured to be able to function in the users desired way.
 
 import logging
 
+import ruamel.yaml.comments
+
 from PyPWA.libs import (process, data, minimizers)
 from PyPWA import VERSION, LICENSE, STATUS
 
@@ -35,6 +38,10 @@ __license__ = LICENSE
 __version__ = VERSION
 
 
+class ConfigurationInitializer(object):
+    pass
+
+
 class Configurator(object):
 
     _builtin_libraries = [process, data, minimizers]
@@ -42,4 +49,99 @@ class Configurator(object):
     def __init__(self, starter):
         self._logger = logging.getLogger(__name__)
         self._logger.addHandler(logging.NullHandler())
-        self._starter = starter
+
+
+class ConfiguratorOptions(object):
+    _options = {
+        # Advanced
+        "plugin directory": "./plugins/",
+        # Optional
+        "logging": "error"
+    }
+
+    _template = {
+        "plugin directory": str,
+        "logging": [
+            "debug", "info", "warning",
+            "error", "critical", "fatal"
+        ]
+    }
+
+    def __init__(self):
+        """
+        Simple Object to hold the options for the Foreman.
+        """
+        header = self._build_empty_options_with_comments()
+        self._optional = self._build_optional(header)
+        self._advanced = self._build_advanced(header)
+        self._required = header
+
+    @staticmethod
+    def _build_empty_options_with_comments():
+        header = ruamel.yaml.comments.CommentedMap()
+        content = ruamel.yaml.comments.CommentedMap()
+
+        header["global"] = content
+        header.yaml_add_eol_comment(
+            "This is the global loaders settings. These settings are the "
+            "options that will be set for the entire program.",
+            "global"
+        )
+
+        content.yaml_add_eol_comment(
+            "This is the option that you would add your own plugins to "
+            "the system. Supported plugins are Data parsing and writing, "
+            "minimization, and even new programs capitalizing on the "
+            "internal frameworking of the program.",
+            "plugin directory"
+        )
+
+        content.yaml_add_eol_comment(
+            "This sets the logging level of the program, supported "
+            "options are debug, info, warning, error, critical, and "
+            "fatal. This setting will be overwritten by the command "
+            "prompt if the user specifies -v",
+            "logging"
+        )
+
+        return header
+
+    def _build_optional(self, header):
+        """
+        Since there is only one option, and its optional, we only have a
+        single building function for the actual options.
+
+        Args:
+            header (ruamel.yaml.comments.CommentedMap): The empty
+                dictionary with the comments included.
+
+        Returns:
+            ruamel.yaml.comments.CommentedMap: The dictionary with the
+                optional fields.
+        """
+        header["global"]["logging"] = self._options["logging"]
+        return header
+
+    def _build_advanced(self, header):
+        options = self._build_optional(header)
+
+        options["global"]["plugin directory"] = \
+            self._options["plugin directory"]
+
+        return options
+
+    @property
+    def return_template(self):
+        return self._template
+
+    @property
+    def return_required(self):
+        return self._required
+
+    @property
+    def return_optional(self):
+        return self._optional
+
+    @property
+    def return_advanced(self):
+        return self._advanced
