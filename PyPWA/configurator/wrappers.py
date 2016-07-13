b65e1c541337366109eb4deeed43b9eb0defd6d8
Mark Jones <maj@jlab.org>
2016-07-13 10:43:26 -0400

    Changed the information that is sent from the wrapper to the starting object.

--- PyPWA/configurator/wrappers.py
+++ PyPWA/configurator/wrappers.py
@@ -81,7 +81,7 @@ class StartProgram(object):
 
             sys.stdout.write("\x1b[2J\x1b[H")
 
-            self.builder.run(application_configuration["Calculation"],
+            self.builder.run(application_configuration,
                              arguments.configuration)
 
         return decorated_builder()
