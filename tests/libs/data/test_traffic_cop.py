adcaa0b1410aeab8f151817c68beab94a1ab50bb
Mark Jones <maj@jlab.org>
2016-08-09 13:28:45 -0400

    Wrote a simple test to load known data from file through the Memory Object.

--- tests/libs/data/test_traffic_cop.py
+++ tests/libs/data/test_traffic_cop.py
@@ -7,13 +7,31 @@ from PyPWA.libs.data import traffic_cop
 
 
 CSV_TEST_DATA = os.path.join(
-    os.path.dirname(__file__), "test_docs/sv_test_data.csv"
+    os.path.dirname(__file__), "builtin/test_docs/sv_test_data.csv"
 )
 
 GAMP_TEST_DATA = os.path.join(
-    os.path.dirname(__file__), "test_docs/gamp_test_data.gamp"
+    os.path.dirname(__file__), "builtin/test_docs/gamp_test_data.gamp"
 )
 
 TEMP_WRITE_LOCATION = os.path.join(
-    os.path.dirname(__file__), "test_docs/temporary_write_data"
+    os.path.dirname(__file__), "builtin/test_docs/temporary_write_data"
 )
+
+
+def test_Memory_ReadData_DataMatches():
+    parser = traffic_cop.Memory(cache=False)
+    data = parser.parse(CSV_TEST_DATA)
+    assert data["QFactor"][3] == 0.832133
+
+#  This will be uncommented as soon as the plugins are fixed.
+#  def test_Memory_LoopingData_DataMatches():
+#      parser = traffic_cop.Memory(cache=False)
+#      data = numpy.zeros(1000, [("data", "f8")])
+#      data["data"] = numpy.random.rand(1000)
+#
+#      parser.write(data, TEMP_WRITE_LOCATION)
+#      new_data = parser.parse(TEMP_WRITE_LOCATION)
+#
+#      numpy.testing.assert_array_equal(new_data, data)
+#      os.remove(TEMP_WRITE_LOCATION)
