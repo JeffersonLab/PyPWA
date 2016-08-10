39650610894ef8df3d81bf84ebb0275efa170afb
Mark Jones <maj@jlab.org>
2016-08-10 10:28:37 -0400

    Removed old plugin loader.

--- PyPWA/libs/data/_utilites.py
+++ PyPWA/libs/data/_utilites.py
@@ -26,7 +26,7 @@ this to get started passing data to it.
 import logging
 import pkgutil
 
-from PyPWA.libs.data import definitions
+from PyPWA.libs.data import templates
 from PyPWA import VERSION, LICENSE, STATUS
 
 __author__ = ["Mark Jones"]
@@ -50,100 +50,6 @@ work even without the new name being defined. Lists can hold names, lists
 can be your friend, especially if you fuzz it.
 """
 MODULE_NAME = "Builtin Parser"  # Name for the module externally.
-
-
-class FindPlugins(object):
-
-    def __init__(self):
-        """
-        Searches packages for modules and packages that contain the
-        metadata needed to start and run the data module.
-        """
-        self._logger = logging.getLogger(__name__)
-        self._logger.addHandler(logging.NullHandler())
-
-    def find_plugin(self, packages):
-        """
-        Starts the search for possible plugins.
-
-        Args:
-            packages (module): The package that might contain the
-                plugin(s)
-
-        Returns:
-            list[module]: The validated plugins that are likely to work
-                with the data module.
-        """
-        possible_plugins = []
-
-        for module in packages:
-            if module:
-                for importer, modname, is_pkg in pkgutil.iter_modules(
-                        module.__path__
-                ):
-
-                    self._logger.debug(
-                        "Found Plugin %s, is package %s" %
-                        (modname, is_pkg)
-                    )
-
-                    plugin = self._import_plugin(importer, modname)
-                    if not isinstance(plugin, bool):
-                        if self._check_metadata(plugin):
-                            possible_plugins.append(plugin)
-
-        return possible_plugins
-
-    def _import_plugin(self, importer, module):
-        """
-        Attempts to import the found plugins.
-
-        Args:
-            importer (importlib.machinery.FileFinder): The initialized
-                importer from pkgutil.
-            module (str): The name of the module that needs to be loaded.
-
-        Returns:
-            module: If the import was successful.
-            False: If the import failed.
-        """
-
-        try:
-            plugin = importer.find_module(module).load_module(module)
-            self._logger.debug("Imported plugin %s" % plugin)
-            return plugin
-        except ImportError as Error:
-
-            self._logger.info(
-                "Import of plugin %s failed with error %s" % module, Error
-            )
-            return False
-
-    def _check_metadata(self, plugin):
-        """
-        Checks for the metadata needed to use the plugin effectively.
-
-        Args:
-            plugin (module): The actual fully imported plugin.
-
-        Returns:
-            True: If the plugin contains metadata.
-            False: If the plugin doesn't have metadata.
-        """
-        if hasattr(plugin, "metadata_data"):
-
-            self._logger.debug(
-                "Plugin %s has metadata, keeping." % plugin.__name__
-            )
-
-            return True
-        else:
-            self._logger.debug(
-                "Plugin %s doesn't have metadata, skipping" %
-                plugin.__name__
-            )
-
-            return False
 
 
 class DataSearch(object):
@@ -189,9 +95,9 @@ class DataSearch(object):
                 self._logger.info("Found %s will load %s" %
                                   (plugin.__name__, file_location))
                 return plugin
-            except definitions.IncompatibleData:
+            except templates.IncompatibleData:
                 self._logger.debug("Skipping %s for data %s" %
                                    (plugin.__name__, file_location))
-        raise definitions.UnknownData(
+        raise templates.UnknownData(
             "Unable to find a plugin that can load %s" % file_location
         )
