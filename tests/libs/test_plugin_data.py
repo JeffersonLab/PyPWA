126a270afed630dbee80ef59f4e9480297bda2ed
Mark Jones <maj@jlab.org>
2016-08-11 02:36:38 -0400

    Added tests to ensure that plugins root objects don't cause issues.

---
+++ tests/libs/test_plugin_data.py
@@ -0,0 +1,33 @@
+from PyPWA.libs import data, minimizers, process
+
+
+def check_for_data(data_object):
+    data_object.request_options("required")
+    data_object.request_options("optional")
+    data_object.request_options("advanced")
+
+    data_object.request_metadata("name")
+    data_object.request_metadata("interface")
+    data_object.request_metadata("provides")
+    data_object.request_metadata("requires function")
+    data_object.request_metadata("arguments")
+
+
+def test_CheckDataIterator_OptionPasses():
+    options = data.DataIterator()
+    check_for_data(options)
+
+
+def test_CheckDataParser_OptionPasses():
+    options = data.DataParser()
+    check_for_data(options)
+
+
+def test_CheckProcess_OptionPasses():
+    options = process.Processing()
+    check_for_data(options)
+
+
+def test_CheckMinimizer_OptionPasses():
+    options = minimizers.MinuitOptions()
+    check_for_data(options)
