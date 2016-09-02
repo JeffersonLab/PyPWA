7e0ab8dfcfcd2114f93b6e19723e997f66072e5e
Mark Jones <maj@jlab.org>
2016-09-02 18:32:16 -0400

    Updated Metadata Storage to use options object, and updated configurator to also use configurator options.

--- PyPWA/configurator/configurator.py
+++ PyPWA/configurator/configurator.py
@@ -26,7 +26,7 @@ import logging
 
 import ruamel.yaml.comments
 
-from PyPWA.configurator import config_loader
+from PyPWA.configurator import config_loader, templates
 import PyPWA.libs
 from PyPWA import VERSION, LICENSE, STATUS
 
@@ -43,19 +43,6 @@ __license__ = LICENSE
 __version__ = VERSION
 
 
-class Configurator(object):
-
-    def __init__(self):
-        self._logger = logging.getLogger(__name__)
-        self._logger.addHandler(logging.NullHandler())
-
-    def build(self, default_config, config_file):
-        reader = config_loader.ConfigReader()
-        configuration = reader.read_config(config_file)
-
-
-
-
 class MetadataStorage(object):
 
     def __init__(self):
@@ -63,7 +50,8 @@ class MetadataStorage(object):
         self._logger.addHandler(logging.NullHandler())
         self._minimization = []
         self._kernel_processing = []
-        self._data = []
+        self._data_reader = []
+        self._data_parser = []
         self._main = []
 
     def add_plugins(self, plugins):
@@ -71,19 +59,33 @@ class MetadataStorage(object):
             self._plugin_filter(plugin)
 
     def _plugin_filter(self, plugin):
-        if plugin["provides"] == "data":
-            self._data.append(plugin)
-        elif plugin["provides"] == "minimization":
-            self._minimization.append(plugin)
-        elif plugin["provides"] == "kernel processing":
-            self._kernel_processing.append(plugin)
-        elif plugin["provides"] == "main":
-            self._main.append(plugin)
+        try:
+            temp_object = plugin()
+            plugin_type = temp_object.request_metadata("provides")
+
+            if plugin_type == "data reader":
+                self._data_reader.append(plugin)
+            elif plugin_type == "data parser":
+                self._data_parser.append(plugin)
+            elif plugin_type == "minimization":
+                self._minimization.append(plugin)
+            elif plugin_type == "kernel processing":
+                self._kernel_processing.append(plugin)
+            elif plugin_type == "main":
+                self._main.append(plugin)
+
+        except Exception as Error:
+            self._logger.error(Error)
 
     def search_plugin(self, plugin_name, plugin_type):
-        if plugin_type is "data":
+        if plugin_type is "data reader":
             return self._plugin_name_search(
-                plugin_name, self._data
+                plugin_name, self._data_reader
+            )
+
+        elif plugin_type is "data parser":
+            return self._plugin_name_search(
+                plugin_name, self._data_parser
             )
 
         elif plugin_type is "minimization":
@@ -124,101 +126,70 @@ class MetadataStorage(object):
         return self._kernel_processing
 
     @property
-    def data(self):
-        return self._data
-
-
-class ConfiguratorOptions(object):
-    _options = {
-        # Advanced
-        "plugin directory": "./plugins/",
-        # Optional
-        "logging": "error"
-    }
-
-    _template = {
-        "plugin directory": str,
-        "logging": [
-            "debug", "info", "warning",
-            "error", "critical", "fatal"
-        ]
-    }
-
-    def __init__(self):
-        """
-        Simple Object to hold the options for the Foreman.
-        """
-        header = self._build_empty_options_with_comments()
-        self._optional = self._build_optional(header)
-        self._advanced = self._build_advanced(header)
-        self._required = header
-
-    @staticmethod
-    def _build_empty_options_with_comments():
-        header = ruamel.yaml.comments.CommentedMap()
-        content = ruamel.yaml.comments.CommentedMap()
-
-        header["global"] = content
-        header.yaml_add_eol_comment(
-            "This is the global loaders settings. These settings are the "
-            "options that will be set for the entire program.",
-            "global"
-        )
-
-        content.yaml_add_eol_comment(
-            "This is the option that you would add your own plugins to "
-            "the system. Supported plugins are Data parsing and writing, "
-            "minimization, and even new programs capitalizing on the "
-            "internal frameworking of the program.",
-            "plugin directory"
-        )
-
-        content.yaml_add_eol_comment(
-            "This sets the logging level of the program, supported "
-            "options are debug, info, warning, error, critical, and "
-            "fatal. This setting will be overwritten by the command "
-            "prompt if the user specifies -v",
-            "logging"
-        )
-
-        return header
-
-    def _build_optional(self, header):
-        """
-        Since there is only one option, and its optional, we only have a
-        single building function for the actual options.
-
-        Args:
-            header (ruamel.yaml.comments.CommentedMap): The empty
-                dictionary with the comments included.
-
-        Returns:
-            ruamel.yaml.comments.CommentedMap: The dictionary with the
-                optional fields.
-        """
-        header["global"]["logging"] = self._options["logging"]
-        return header
-
-    def _build_advanced(self, header):
-        options = self._build_optional(header)
-
-        options["global"]["plugin directory"] = \
-            self._options["plugin directory"]
-
-        return options
+    def data_reader(self):
+        return self._data_reader
 
     @property
-    def return_template(self):
-        return self._template
+    def data_parser(self):
+        return self._data_parser
 
-    @property
-    def return_required(self):
-        return self._required
 
-    @property
-    def return_optional(self):
-        return self._optional
+class ConfiguratorOptions(templates.OptionsTemplate):
 
-    @property
-    def return_advanced(self):
-        return self._advanced
+    def _plugin_name(self):
+        return "Global Options"
+
+    def _plugin_interface(self):
+        return False
+
+    def _plugin_type(self):
+        return False
+
+    def _plugin_arguments(self):
+        return False
+
+    def _plugin_requires(self):
+        return False
+
+    def _default_options(self):
+        return {
+            "plugin directory": "none",
+            "logging": [
+                "debug", "info", "warning",
+                "error", "critical", "fatal"
+            ]
+        }
+
+    def _option_levels(self):
+        return {
+            "plugin directory": self._advanced,
+            "logging": self._optional
+        }
+
+    def _option_types(self):
+        return {
+            "plugin directory": str,
+            "logging": [
+                "debug", "info", "warning",
+                "error", "critical", "fatal"
+            ]
+        }
+
+    def _option_comments(self):
+        return {
+            "plugin directory":
+                "This is the option that you would add your own plugins "
+                "to the system. Supported plugins are Data parsing and "
+                "writing, minimization, and even new programs "
+                "capitalizing on the internal frameworking of "
+                "the program.",
+            "logging":
+                "This sets the logging level of the program, supported "
+                "options are debug, info, warning, error, critical, and "
+                "fatal. This setting will be overwritten by the command "
+                "prompt if the user specifies -v"
+        }
+
+    def _main_comment(self):
+        return "This is the global loaders settings. These settings " \
+               "are the options that will be set for the entire program."
