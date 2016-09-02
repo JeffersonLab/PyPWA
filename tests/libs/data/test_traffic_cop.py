138564421b318253c4aff60a936a136caba4c642
Mark Jones <maj@jlab.org>
2016-09-02 13:21:59 -0400

    Added tests for the Iterator object.

--- tests/libs/data/test_traffic_cop.py
+++ tests/libs/data/test_traffic_cop.py
@@ -1,8 +1,9 @@
-import os
+import collections
+
 import numpy
+import os
 
 from PyPWA.libs.data import traffic_cop
-
 
 CSV_TEST_DATA = os.path.join(
     os.path.dirname(__file__), "builtin/test_docs/sv_test_data.csv"
@@ -55,3 +56,37 @@ def test_MemoryLoopingDataWithCache_DataMatches():
     clearer = traffic_cop.Memory(False, True)
     clearer.parse(TEMP_WRITE_LOCATION)
     os.remove(TEMP_WRITE_LOCATION)
+
+
+def test_Iterator_ReadData_DataMatches():
+    handler = traffic_cop.Iterator()
+    empty_reader = handler.return_reader(CSV_TEST_DATA)
+    reader = empty_reader(CSV_TEST_DATA)
+    first_line = reader.next()
+    assert first_line["ctAD"] == -0.265433
+
+
+def test_Iterator_LoopingData_DataMatches():
+    handler = traffic_cop.Iterator()
+
+    particle = numpy.zeros(1, [("x", "f8"), ("y", "f8")])
+    data = collections.deque()
+    for number in range(10):
+        particle["x"] = numpy.random.rand()
+        particle["y"] = numpy.random.rand()
+        data.append(particle)
+
+    unloaded_writer = handler.return_writer(TEMP_WRITE_LOCATION, 1)
+    writer = unloaded_writer(TEMP_WRITE_LOCATION)
+    for event in data:
+        writer.write(event)
+    writer.close()
+
+    unloaded_reader = handler.return_reader(TEMP_WRITE_LOCATION)
+    reader = unloaded_reader(TEMP_WRITE_LOCATION)
+    for event in data:
+        new_event = reader.next_event
+        numpy.testing.assert_array_equal(new_event, event)
+    reader.close()
+    os.remove(TEMP_WRITE_LOCATION)
+
