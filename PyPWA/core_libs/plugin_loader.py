ebd539fd44bf0c777c0a8cee18182e102fd47b3b
Mark Jones <maj@jlab.org>
2016-09-12 04:49:12 -0400

    Added an object to load single functions by name from a single python sheet.

--- PyPWA/core_libs/plugin_loader.py
+++ PyPWA/core_libs/plugin_loader.py
@@ -137,3 +137,26 @@ class PluginLoading(object):
         return plugins
 
 
+class SingleFunctionLoader(object):
+    def __init__(self, file):
+        self._logger = logging.getLogger(__name__)
+        self._logger.addHandler(logging.NullHandler())
+        self._module = None  # type: type(PyPWA)
+
+        self._load_module(file)
+
+    def _load_module(self, file):
+        sys.path.append(
+            os.path.dirname(os.path.abspath(file))
+        )
+
+        self._module = importlib.import_module(
+            os.path.splitext(os.path.basename(file))[0]
+        )
+
+    def fetch_function(self, function_name, fail=False):
+        try:
+            return getattr(self._module, function_name)
+        except Exception:
+            if fail:
+                raise
