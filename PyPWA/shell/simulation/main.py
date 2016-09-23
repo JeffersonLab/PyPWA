cfc7129aac90c6775ea77634ce909918c71bcdb7
Mark Jones <maj@jlab.org>
2016-09-23 13:51:41 -0400

    Added intial simulation module.

---
+++ PyPWA/shell/simulation/main.py
@@ -0,0 +1,124 @@
+import numpy
+
+from PyPWA.core_libs import plugin_loader
+from PyPWA.core_libs.templates import plugin_templates
+from PyPWA.core_libs.templates import interface_templates
+
+class Simulator(plugin_templates.ShellMain):
+
+    def __init__(
+            self, data_parser=None, kernel_processing=None,
+            the_type=None, functions_location=None,
+            processing_name=None, setup_name=None, data_location=None,
+            parameters=None, max_intensity=None, save_name=None,
+            options=None
+    ):
+        """
+
+        Args:
+            data_parser (plugin_templates.DataParserTemplate):
+            kernel_processing (plugin_templates.KernelProcessingTemplate):
+            the_type (str):
+            functions_location (str):
+            processing_name (str):
+            setup_name (str):
+            data_location (str):
+            parameters (dict):
+            max_intensity (numpy.float64):
+            save_name (str):
+            options (dict):
+        """
+
+        self._data_parser = data_parser
+        self._kernel_processing = kernel_processing
+        self._the_type = the_type
+        self._functions_location = functions_location
+        self._processing_name = processing_name
+        self._setup_name = setup_name
+        self._data_location = data_location
+        self._parameters = parameters
+        self._max_intensity = max_intensity
+        self._save_name = save_name
+
+        if options:
+            super(Simulator, self).__init__(options)
+
+        self._raw_data = {}
+        self._intensities = None  # type: numpy.ndarray
+
+    def start(self):
+        if self._the_type == "full" or self._the_type == "intensities":
+            self._calc_intensities()
+        if self._the_type == "full" or self._the_type == "weighting":
+            self._rejection_method()
+
+    def _calc_intensities(self):
+        loader = plugin_loader.SingleFunctionLoader(
+            self._functions_location
+        )
+
+        processing = loader.fetch_function(self._processing_name)
+        setup = loader.fetch_function(self._setup_name)
+
+        self._raw_data["data"] = self._data_parser.parse(
+            self._data_location
+        )
+
+        the_kernel = IntensityKernel(setup, processing, self._parameters)
+        the_interface = IntensityInterface()
+
+        self._kernel_processing.main_options(
+            self._raw_data, the_kernel, the_interface
+        )
+
+        operational_interface = self._kernel_processing.fetch_interface()
+        final_value = operational_interface.run()
+
+
+    def _rejection_method(self):
+
+
+
+
+class IntensityInterface(interface_templates.AbstractInterface):
+
+    is_duplex = False
+
+    def run(self, communicator, args):
+        """
+
+        Args:
+            communicator (list):
+            args:
+
+        Returns:
+
+        """
+
+        final_array = numpy.zeros(len(communicator))
+
+        for index, communication in enumerate(communicator):
+            final_array[index] = communication.recv()
+
+        return final_array
+
+class IntensityKernel(interface_templates.AbstractKernel):
+
+    def __init__(self, setup_function, processing_function, parameters):
+        """
+
+        Args:
+            setup_function:
+            processing_function:
+            parameters:
+        """
+        self._setup_function = setup_function
+        self._processing_function = processing_function
+        self._parameters = parameters
+        self.data = None  # type: numpy.ndarray
+
+    def setup(self):
+        self._setup_function()
+
+    def process(self, data=False):
+        return self._processing_function(self._parameters, self.data)
