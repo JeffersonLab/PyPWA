a278737ec54779414647cbbf535710471ee0d83d
Mark Jones <maj@jlab.org>
2016-09-26 13:48:26 -0400

    Replaced MakeConfiguration with SimpleConfigBuilder and PluginList

--- PyPWA/configurator/config_loader.py
+++ PyPWA/configurator/config_loader.py
@@ -14,17 +14,17 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import io
 import logging
 import sys
 
 import PyPWA.libs
 import PyPWA.shell
 import fuzzywuzzy.process
-import io
 import os
 import ruamel.yaml
-import ruamel.yaml.parser
 import ruamel.yaml.comments
+import ruamel.yaml.parser
 from PyPWA import VERSION, LICENSE, STATUS
 from PyPWA.configurator import _storage
 from PyPWA.core_libs import plugin_loader
@@ -89,7 +89,8 @@ class ConfigParser(object):
                 raise SyntaxError(str(UserError))
 
 
-class MakeConfiguration(object):
+class SimpleConfigBuilder(object):
+
     def __init__(self):
         self._logger = logging.getLogger(__name__)
         self._logger.addHandler(logging.NullHandler())
@@ -99,129 +100,110 @@ class MakeConfiguration(object):
         )
 
         self._input_manager = SimpleInputObject()
+        self._settings = ruamel.yaml.comments.CommentedMap()
 
-        self._settings = {}
+        self._storage = None  # type: _storage.MetadataStorage
+        self._plugin_directory = None  # type: str
+        self._save_location = None  # type: str
+        self._level = None  # type: str
+        self._plugins = None  # type: list
 
-        self._save_location = ""
-        self._level = ""
-
-    def make_configuration(
+    def build_configuration(
             self, plugin_name, main_plugin,
-            provided_options=None, save_location=""
+            provided_options=None, save_location=False
     ):
-        """
+        self._determine_plugin_level()
+        self._determine_plugin_directory()
+        self._build_storage()
+        self._make_plugin_list(main_plugin)
+        self._build_configuration()
+        self._add_main_to_configuration(main_plugin)
+        self._correct_options(main_plugin, plugin_name, provided_options)
+        self._set_save_location(save_location)
 
-        Args:
-            plugin_name (str):
-            main_plugin (option_templates.MainOptionsTemplate):
-            provided_options (list):
-            save_location (str):
+    def _determine_plugin_level(self):
+        possible_levels = ["required", "optional", "advanced"]
 
-        Returns:
+        question = "\nHow much control would you like to have over the " \
+                   "configuration? \nrequired\noptional (default, " \
+                   "recommended)\nadvanced\n\n[optional]: "
 
-        """
-        plugin_directory = None
+        self._level = self._input_manager.input(
+            question, possible_levels, "optional"
+        )
 
-        self._save_location = save_location
-        self._request_level()
+    def _determine_plugin_directory(self):
+        question = "\nWould you like to use your own plugins? If YES " \
+                   "then please enter the location, if NO then just " \
+                   "press ENTER.\n[None]: "
+
         if self._level == "advanced":
-            plugin_directory = self._request_plugin()
-
-        plugins = self._plugin_handler.fetch_plugin(
-            [PyPWA.libs, plugin_directory]
-        )
-
-        storage = _storage.MetadataStorage()
-        storage.add_plugins(plugins)
-
-        selected_plugins = self._parse_plugins(main_plugin, storage)
-        configuration = self._build_configuration(selected_plugins)
-        full_configuration = self._add_main_plugin_to_configuration(
-            configuration, main_plugin
-        )
-
-        fixed_config = self._correct_main_name(
-            main_plugin, plugin_name, full_configuration, provided_options
-        )
-
-        self._write_config(fixed_config)
-
-    def _add_main_plugin_to_configuration(
-            self, configuration, main_plugin
-    ):
-        configuration[main_plugin.request_metadata("id")] = \
-            main_plugin.request_options(self._level)
-
-        return configuration
-
-    def _write_config(self, configuration):
-        string = """\
-What would you like to name the configuration file?
-[File Name?]: """
-
-        if self._save_location == "":
-            self._save_location = self._input_manager.input(string)
-
-        with io.open(self._save_location, "w") as stream:
-            stream.write(
-                ruamel.yaml.dump(
-                    configuration,
-                    Dumper=ruamel.yaml.RoundTripDumper
-                )
+            self._plugin_directory = self._input_manager.input(
+                question, default_answer="None", is_dir=True
             )
+        else:
+            self._plugin_directory = None
 
-    @staticmethod
-    def _correct_main_name(
-            main_plugin, main_name, configuration, provided_options=None
+    def _build_storage(self):
+        plugins = self._plugin_handler.fetch_plugin(
+            [PyPWA.libs, self._plugin_directory]
+        )
+
+        self._storage = _storage.MetadataStorage()
+        self._storage.add_plugins(plugins)
+
+    def _make_plugin_list(self, main_plugin):
+        list_maker = PluginList()
+        self._plugins = list_maker.parse_plugins(
+            main_plugin, self._storage
+        )
+
+    def _build_configuration(self):
+        configuration = ruamel.yaml.comments.CommentedMap()
+        for plugin in self._plugins:
+            configuration.update(plugin.request_options(self._level))
+        self._settings = configuration
+
+    def _add_main_to_configuration(self, main_plugin):
+        self._settings.update(main_plugin.request_options(self._level))
+
+    def _correct_options(
+            self, main_plugin, main_name, provided_options
     ):
-        """
-
-        Args:
-            main_plugin (option_templates.MainOptionsTemplate):
-            main_name (str):
-            configuration (dict):
-
-        Returns:
-
-        """
         shell_id = main_plugin.request_metadata("id")
 
         if provided_options:
             for option in provided_options:
-                configuration[shell_id].pop(option)
+                self._settings[shell_id].pop(option)
 
-        configuration[main_name] = \
-            configuration[shell_id]
+        self._settings[main_name] = self._settings[shell_id]
+        self._settings.pop(shell_id)
 
-        configuration.pop(shell_id)
+    def _set_save_location(self, save_location=False):
+        question = "\nWhat would you like to name the configuration " \
+                   "file?\n[File Name?]: "
 
-        return configuration
+        if save_location:
+            self._save_location = save_location
+        else:
+            self._save_location = self._input_manager.input(question)
 
-    def _build_configuration(self, plugin_list):
-        """
+    def _write_final_config(self):
+        with io.open(self._save_location, "w") as stream:
+            stream.write(ruamel.yaml.dump(
+                self._settings, Dumper=ruamel.yaml.RoundTripDumper
+            ))
 
-        Args:
-            plugin_list (list[option_templates.PluginsOptionsTemplate]):
 
-        Returns:
+class PluginList(object):
 
-        """
-        configuration = ruamel.yaml.comments.CommentedMap()
-        for plugin in plugin_list:
-            configuration.update(plugin.request_options(self._level))
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.addHandler(logging.NullHandler())
 
-        return configuration
+        self._input_manager = SimpleInputObject()
 
-    def _parse_plugins(self, main_plugin, storage):
-        """
-
-        Args:
-            main_plugin (option_templates.MainOptionsTemplate):
-            storage (_storage.MetadataStorage):
-
-        Returns:
-
-        """
+    def parse_plugins(self, main_plugin, storage):
         plugins = []
         if main_plugin.requires("data parser"):
             plugins.append(self._process_plugins(
@@ -252,23 +234,11 @@ What would you like to name the configuration file?
     def _process_plugins(
             self, plugin_type, plugin_type_name, storage, plugin_list
     ):
-        """
-
-        Args:
-            plugin_type (str):
-            plugin_type_name (str):
-            storage (_storage.MetadataStorage):
-            plugin_list (list):
-
-        Returns:
-
-        """
-
         if len(plugin_list) == 1:
             empty_plugin = plugin_list[0]
         else:
             name = self._ask_plugin(
-                storage.data_parser, plugin_type_name
+                plugin_list, plugin_type_name
             )
 
             empty_plugin = storage.search_plugin(name, plugin_type)
@@ -276,21 +246,13 @@ What would you like to name the configuration file?
         return empty_plugin()
 
     def _ask_plugin(self, plugin_list, plugin_type):
-        """
-
-        Args:
-            plugin_list (list[option_templates.PluginsOptionsTemplate]):
-
-        Returns:
-
-        """
         names = []
         for plugin in plugin_list:
-            names.append(plugin.request_metadata("name"))
+            the_plugin = plugin()
+            names.append(the_plugin.request_metadata("name"))
 
-        string = """\
-Which would plugin would you like to use for {0}?
-""".format(plugin_type)
+        string = "Which would plugin would you " \
+                 "like to use for {0}?".format(plugin_type)
 
         for name in names:
             string += "\n{0}".format(name)
@@ -299,38 +261,10 @@ Which would plugin would you like to use for {0}?
 
         return self._input_manager.input(string, possible_answers=names)
 
-    def _request_plugin(self):
-        string = """
-Would you like to use your own plugins? If YES then please enter the
-location, if NO then just press ENTER.
-[None]: """
-
-        return self._input_manager.input(string, is_dir=True)
-
-    def _request_level(self):
-        string = """\
-How much control would you like to have over the configuration?
-required
-optional (default, recommended)
-advanced
-
-[optional]: """
-
-        levels = ["required", "optional", "advanced"]
-
-        self._level = self._input_manager.input(
-            string, possible_answers=levels, default_answer="optional"
-        )
-
 
 class SimpleInputObject(object):
 
     def __init__(self, auto_correct_percentage=75):
-        if sys.version_info.major == "2":
-            self.__input = raw_input
-        else:
-            self.__input = input
-
         self._auto_correction_percentage = auto_correct_percentage
 
     def input(
@@ -366,13 +300,14 @@ class SimpleInputObject(object):
                     if corrected_answer[1] > \
                             self._auto_correction_percentage:
                         answer = corrected_answer[0]
-
                 if is_dir:
                     if not os.path.isdir(answer):
                         continue
                 if self._is_correct(answer):
                     final_answer = answer
 
+        if final_answer == "None":
+            final_answer = None
         return final_answer
 
     def _is_correct(self, answer):
@@ -398,3 +333,10 @@ It looks like you selected '{0}', is this correct?
             return True
         elif final_answer == "no":
             return False
+
+    @staticmethod
+    def __input(string):
+        if sys.version_info.major == "2":
+            return raw_input(string)
+        else:
+            return input(string)
