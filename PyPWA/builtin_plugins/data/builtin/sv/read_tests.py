37d5d98937d2185ef18aad8edcff45fe8df86787
Mark Jones <maj@jlab.org>
2016-10-31 13:44:41 -0400

    Removed an unneeded test.

--- PyPWA/builtin_plugins/data/builtin/sv/read_tests.py
+++ PyPWA/builtin_plugins/data/builtin/sv/read_tests.py
@@ -40,12 +40,10 @@ class SvDataTest(data_templates.ReadTest):
     def quick_test(self, file_location):
         self._set_stream(file_location)
         self._header_test()
-        self._comparison_test(5)
 
     def full_test(self, file_location):
         self._set_stream(file_location)
         self._header_test()
-        self._comparison_test()
 
     def _set_stream(self, file_location):
         self._stream = io.open(file_location, "r")
@@ -53,10 +51,6 @@ class SvDataTest(data_templates.ReadTest):
     def _header_test(self):
         header_test = _HeaderTest(self._stream)
         header_test.do_test()
-
-    def _comparison_test(self, lines=0):
-        comparison_test = _CountTest(self._stream, lines)
-        comparison_test.do_test()
 
 
 class _HeaderTest(object):
@@ -95,75 +89,7 @@ class _HeaderTest(object):
             return self._sniffer.has_header(self._sample)
         except Exception as Error:
             self._logger.error(Error)
-            raise exceptions.IncompatibleData
-
-    def _reset_stream(self):
-        self._stream.seek(0)
-
-
-class _CountTest(object):
-
-    _logger = logging.getLogger(__name__)
-    _stream = None  # type: io.FileIO
-    _comma_count = 0
-    _tab_count = 0
-    _break_count = 0
-
-    def __init__(self, stream, count):
-        self._logger.addHandler(logging.NullHandler())
-        self._stream = stream
-        self._break_count = count
-
-    def do_test(self):
-        self._count_test()
-        self._reset_stream()
-
-    def _count_test(self):
-        for index, line in enumerate(self._stream):
-            if self._should_break(index):
-                break
-            self._begin_test(line)
-
-    def _should_break(self, index):
-        if not self._break_count == 0 and self._break_count == index:
-            return True
-        else:
             return False
-
-    def _begin_test(self, line):
-        if self._zeroed():
-            self._set_counts(line)
-        self._do_comparison(line)
-
-    def _zeroed(self):
-        if self._comma_count == 0 and self._tab_count == 0:
-            return True
-        else:
-            return False
-
-    def _set_counts(self, line):
-        self._tab_count = len(line.split("\t"))
-        self._comma_count = len(line.split(","))
-
-    def _do_comparison(self, line):
-        if not self._tab_count == 0:
-            self._compare_tabs(line)
-        elif not self._comma_count == 0:
-            self._compare_commas(line)
-        else:
-            raise exceptions.IncompatibleData
-
-    def _compare_tabs(self, line):
-        self._compare_counts(line, "\t", self._tab_count)
-
-    def _compare_commas(self, line):
-        self._compare_counts(line, ",", self._comma_count)
-
-    @staticmethod
-    def _compare_counts(line, delimiter, original_count):
-        count = len(line.split(delimiter))
-        if not count == original_count:
-            raise exceptions.IncompatibleData
 
     def _reset_stream(self):
         self._stream.seek(0)
