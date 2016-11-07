49355ba450bbf522b4b15e238852fa89b6bc1d4b
Mark Jones <maj@jlab.org>
2016-11-07 15:31:25 -0500

    Refactored sv iterator to be more readable.

--- PyPWA/builtin_plugins/data/builtin/sv/s_iterator.py
+++ PyPWA/builtin_plugins/data/builtin/sv/s_iterator.py
@@ -35,36 +35,54 @@ HEADER_SEARCH_BITS = 1024
 
 class SvReader(interface_templates.ReaderInterfaceTemplate):
 
+    _previous_event = None  # type: collections.namedtuple
+    _reader = False  # type: csv.DictReader
+    _file = False  # type: io.TextIOBase
+    _types = False  # type: list[tuple]
+    _elements = False  # type: list[str]
+
     def __init__(self, file_location):
         super(SvReader, self).__init__(file_location)
-        self._previous_event = None  # type: collections.namedtuple
-        self._reader = False  # type: csv.DictReader
-        self._file = False  # type: io.TextIOBase
-        self._types = False  # type: list[tuple]
-        self._elements = False  # type: list[str]
-        self._file_location = file_location
-
+        self._set_file_location(file_location)
         self._start_input()
 
-    def _start_input(self):
-        if self._file:
-            self._file.close()
+    def _set_file_location(self, file_location):
+        self._file_location = file_location
 
+    def _start_input(self):
+        self._set_file()
+        dialect = self._get_dialect()
+        self._set_reader(dialect)
+        self._set_elements()
+        self._set_numpy_types()
+
+    def _set_file(self):
         self._file = io.open(self._file_location)
+
+    def _get_dialect(self):
         dialect = csv.Sniffer().sniff(
             self._file.read(HEADER_SEARCH_BITS), delimiters=[",", "\t"]
         )
         self._file.seek(0)
+        return dialect
 
+    def _set_reader(self, dialect):
         self._reader = csv.reader(self._file, dialect)
 
+    def _set_elements(self):
         self._elements = next(self._reader)
+
+    def _set_numpy_types(self):
         self._types = []
         for element in self._elements:
             self._types.append((element, "f8"))
 
     def reset(self):
+        self.close()
         self._start_input()
+
+    def close(self):
+        self._file.close()
 
     @property
     def next_event(self):
@@ -82,44 +100,62 @@ class SvReader(interface_templates.ReaderInterfaceTemplate):
     def previous_event(self):
         return self._previous_event
 
-    def close(self):
-        self._file.close()
-
 
 class SvWriter(interface_templates.WriterInterfaceTemplate):
+
+    _dialect = csv.Dialect
+    _writer = csv.DictWriter
+    _field_names = [str]
 
     def __init__(self, file_location):
         super(SvWriter, self).__init__(file_location)
         self._file = open(file_location, "w")
-        extension = file_location.split(".")[-1]
+        self._set_dialect(file_location)
 
-        if extension == "tsv":
+    def _set_dialect(self, file_location):
+        if self._is_tab(file_location):
             self._dialect = csv.excel_tab
         else:
             self._dialect = csv.excel
 
-        self._writer = False  # type: cvs.DictWriter
-        self._field_names = False  # type: list[str]
+    def _is_tab(self, file_location):
+        return self._get_extension(file_location) == ".tsv"
 
-    def _writer_setup(self, data):
-        if not self._writer:
-            self._field_names = list(data.dtype.names)
-
-            self._writer = csv.DictWriter(
-                self._file,
-                fieldnames=self._field_names,
-                dialect=self._dialect
-            )
-
-            self._writer.writeheader()
+    @staticmethod
+    def _get_extension(file_location):
+        return file_location.split(".")[-1]
 
     def write(self, data):
         self._writer_setup(data)
+        converted_data = self._convert_to_dict(data)
+        self._write_row(converted_data)
 
+    def _writer_setup(self, data):
+        if not self._writer:
+            self._set_field_names(data)
+            self._set_writer()
+            self._write_header()
+
+    def _set_field_names(self, data):
+        self._field_names = list(data.dtype.names)
+
+    def _set_writer(self):
+        self._writer = csv.DictWriter(
+            self._file,
+            fieldnames=self._field_names,
+            dialect=self._dialect
+        )
+
+    def _write_header(self):
+        self._writer.writeheader()
+
+    def _convert_to_dict(self, data):
         dict_data = {}
         for field_name in self._field_names:
             dict_data[field_name] = repr(data[0][field_name])
+        return dict_data
 
+    def _write_row(self, dict_data):
         self._writer.writerow(dict_data)
 
     def close(self):
