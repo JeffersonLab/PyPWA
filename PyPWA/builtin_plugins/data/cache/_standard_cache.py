26c05f02ba408b3884b7cc48dc9ddf9c82ca6bcd
Mark Jones <maj@jlab.org>
2016-12-02 17:25:14 -0500

    Added more error checking.

--- PyPWA/builtin_plugins/data/cache/_standard_cache.py
+++ PyPWA/builtin_plugins/data/cache/_standard_cache.py
@@ -73,10 +73,12 @@ class ReadCache(_template.ReadInterface):
         try:
             returned_data = self._load_data()
             self._logger.info("Successfully loaded pickle cache!")
-        except (OSError, IOError, KeyError):
+        except (OSError, IOError):
             returned_data = self._empty_raw_data
             self._logger.info("No cache exists.")
-        except (pickle.PickleError, ValueError) as Error:
+        except (
+                pickle.PickleError, ValueError, IndexError, KeyError
+        ) as Error:
             returned_data = self._empty_raw_data
             self._logger.info(
                 "Pickle is from a different Python version or is damaged."
