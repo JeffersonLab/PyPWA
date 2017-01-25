52cf5a53702a411c787d3b98d7e55b60871431d2
Mark Jones <maj@jlab.org>
2017-01-25 12:26:17 -0500

    Updated tests for more coverage.

--- tests/core/test_plugin_loader.py
+++ tests/core/test_plugin_loader.py
@@ -1,22 +1,80 @@
+import os
+
+import pytest
+
 from PyPWA import builtin_plugins
-from PyPWA.builtin_plugins import data, process, minuit
+from PyPWA.builtin_plugins import data, process, minuit, nestle
 from PyPWA.core import plugin_loader
 from PyPWA.core.templates import option_templates
 
+EXAMPLE_SHEET = os.path.join(
+    os.path.dirname(__file__), "example_python_sheet.py"
+)
 
-def test_PluginLoading_ImportsPlugins_FindsAllLibs():
-    """
-    Ensures that the PluginLoader finds all the plugins when supplied
-    with a module and nothing more.
-    """
+DOES_NOT_EXIST = os.path.join(
+    os.path.dirname(__file__), "awfulness.py"
+)
+
+@pytest.fixture(scope="module")
+def plugin_loader_with_plugins():
     loader = plugin_loader.PluginLoading(
         option_templates.PluginsOptionsTemplate
     )
-    plugins = loader.fetch_plugin([builtin_plugins])
+    return loader.fetch_plugin([builtin_plugins])
 
-    assert data.DataIterator in plugins
-    assert data.DataParser in plugins
-    assert process.Processing in plugins
-    assert minuit.MinuitOptions in plugins
 
-    assert len(plugins) == 4 or len(plugins) == 5
+def test_data_iterator_is_found(plugin_loader_with_plugins):
+    assert data.DataIterator in plugin_loader_with_plugins
+
+
+def test_data_parser_is_found(plugin_loader_with_plugins):
+    assert data.DataParser in plugin_loader_with_plugins
+
+
+def test_processing_is_found(plugin_loader_with_plugins):
+    assert process.Processing in plugin_loader_with_plugins
+
+
+def test_minuit_is_found(plugin_loader_with_plugins):
+    assert minuit.MinuitOptions in plugin_loader_with_plugins
+
+
+def test_nestle_is_found(plugin_loader_with_plugins):
+    assert nestle.NestleOptions in plugin_loader_with_plugins
+
+
+@pytest.fixture(scope="module")
+def python_sheet_loader():
+    loader = plugin_loader.PythonSheetLoader(EXAMPLE_SHEET)
+    return loader
+
+
+def test_finds_meaning(python_sheet_loader):
+    fun = python_sheet_loader.fetch_function("the_meaning_of_life")
+    assert fun() == 42
+
+
+def test_finds_internet(python_sheet_loader):
+    fun = python_sheet_loader.fetch_function("what_is_the_internet_ran_by")
+    assert fun() == "cats"
+
+
+def test_finds_console(python_sheet_loader):
+    fun = python_sheet_loader.fetch_function("the_best_gaming_console")
+    assert fun("microsoft") == "linux"
+    assert fun() == "sony"
+
+
+def test_cant_find_nothing(python_sheet_loader):
+    with pytest.raises(AttributeError):
+        fun = python_sheet_loader.fetch_function("nothing", True)
+
+
+def test_returns_empty(python_sheet_loader):
+    fun = python_sheet_loader.fetch_function("nothing", False)
+    assert isinstance(fun(), type(None))
+
+
+def test_can_load_non_existant_file():
+    with pytest.raises(ImportError):
+        loader = plugin_loader.PythonSheetLoader(DOES_NOT_EXIST)
