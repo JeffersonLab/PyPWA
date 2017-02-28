196ff0dc86ae88cdd86f8a991c810dc8bd666bf4
Mark Jones <maj@jlab.org>
2017-02-28 16:07:36 -0500

    Updated InputManager to be a little more clear.

--- PyPWA/core/configurator/_input.py
+++ PyPWA/core/configurator/_input.py
@@ -32,80 +32,74 @@ __version__ = VERSION
 
 class _Input(object):
 
-    _version = sys.version_info.major
+    __version = sys.version_info.major
 
     def input(self, string):
-        if self._version == 2:
-            return self._python_2(string)
+        if self.__version == 2:
+            return self.__python_2(string)
         else:
-            return self._python_new(string)
+            return self.__python_new(string)
 
     @staticmethod
-    def _python_2(string):
+    def __python_2(string):
         return raw_input(string)
 
     @staticmethod
-    def _python_new(string):
+    def __python_new(string):
         return input(string)
 
 
 class _IsCorrect(object):
 
-    _QUERY = "It looks like you selected '{0}', is this correct?\n" + \
+    __QUERY = "It looks like you selected '{0}', is this correct?\n" + \
              "[Y]es/No: "
 
-    _input = _Input
-    _auto_correct_percentage = None
-    _input_data = None
-    _processed_answer = None
+    __input = _Input
+    __auto_correct_percentage = None
+    __input_data = None
+    __processed_answer = None
 
     def __init__(self, auto_correct_percentage=90):
-        self._input = _Input()
-        self._auto_correct_percentage = auto_correct_percentage
+        self.__input = _Input()
+        self.__auto_correct_percentage = auto_correct_percentage
 
     def ask(self, value):
-        self._question_loop(value)
-        return self._process_final_answer()
+        self.__question_loop(value)
+        return self.__process_final_answer()
 
-    def _question_loop(self, value):
+    def __question_loop(self, value):
         while True:
-            self._get_value(value)
+            self.__get_value(value)
 
-            if self._is_blank():
-                self._set_answer("yes")
+            if self._input_data == "":
+                self.__set_answer("yes")
                 break
 
-            self._fuzz_value()
-            if self._answer_is_valid():
+            self.__fuzz_value()
+            if self.__answer_is_valid():
                 break
 
-    def _get_value(self, value):
-        input_data = self._input.input(self._QUERY.format(value))
+    def __get_value(self, value):
+        input_data = self.__input.input(self.__QUERY.format(value))
         self._input_data = input_data.lower()
 
-    def _is_blank(self):
-        if self._input_data == "":
-            return True
-        else:
-            return False
-
-    def _set_answer(self, value):
+    def __set_answer(self, value):
         self._processed_answer = [value, 100]
 
-    def _fuzz_value(self):
+    def __fuzz_value(self):
         choices = ["yes", "no"]
 
         self._processed_answer = fuzzywuzzy.process.extractOne(
             self._input_data, choices
         )
 
-    def _answer_is_valid(self):
-        if self._processed_answer[1] > self._auto_correct_percentage:
+    def __answer_is_valid(self):
+        if self._processed_answer[1] > self.__auto_correct_percentage:
             return True
         else:
             return False
 
-    def _process_final_answer(self):
+    def __process_final_answer(self):
         if self._processed_answer[0] == "yes":
             return True
         elif self._processed_answer[0] == "no":
@@ -114,12 +108,12 @@ class _IsCorrect(object):
 
 class SimpleInputObject(object):
 
-    _input = _Input
-    _is_correct = _IsCorrect
+    __input = _Input
+    __is_correct = _IsCorrect
 
-    _auto_correction_percentage = None
-    _users_input = None
-    _processed_value = None
+    __auto_correction_percentage = None
+    __users_input = None
+    __processed_value = None
 
     def __init__(self, auto_correct_percentage=75):
         self._auto_correction_percentage = auto_correct_percentage
@@ -130,66 +124,48 @@ class SimpleInputObject(object):
             self, string, possible_answers=False,
             default_answer=False, is_dir=False
     ):
-        self._question_loop(string, possible_answers, default_answer, is_dir)
+        self.__question_loop(string, possible_answers, default_answer, is_dir)
         return self._processed_value[0]
 
-    def _question_loop(
+    def __question_loop(
             self, string, possible_answers=False,
             default_answer=False, is_dir=False
     ):
 
         while True:
-            self._get_input(string)
+            self.__get_input(string)
 
-            if self._answer_is_blank():
+            if self.__users_input is "":
                 if default_answer:
-                    self._set_processed_value(default_answer)
+                    self.__set_processed_value(default_answer)
                     break
                 continue
 
             else:
                 if possible_answers:
-                    self._process_value(possible_answers)
+                    self.__process_value(possible_answers)
 
-                    if self._answer_is_valid():
+                    if self.__answer_is_valid():
                         continue
                 if is_dir:
-                    if not self._answer_is_directory():
+                    if not os.path.isdir(self._processed_value[0]):
                         continue
-                if self._answer_is_correct():
+                if self._is_correct.ask(self._processed_value[0]):
                     break
 
-    def _get_input(self, string):
+    def __get_input(self, string):
         self._users_input = self._input.input(string)
 
-    def _answer_is_blank(self):
-        if self._users_input is "":
-            return True
-        else:
-            return False
-
-    def _set_processed_value(self, value):
+    def __set_processed_value(self, value):
         self._processed_value = [value, 100]
 
-    def _process_value(self, possible_answers):
+    def __process_value(self, possible_answers):
         self._processed_value = fuzzywuzzy.process.extractOne(
             self._users_input, possible_answers
         )
 
-    def _answer_is_valid(self):
+    def __answer_is_valid(self):
         if self._processed_value[1] > self._auto_correction_percentage:
-            return True
-        else:
-            return False
-
-    def _answer_is_directory(self):
-        if os.path.isdir(self._processed_value[0]):
-            return True
-        else:
-            return False
-
-    def _answer_is_correct(self):
-        if self._is_correct.ask(self._processed_value[0]):
             return True
         else:
             return False
