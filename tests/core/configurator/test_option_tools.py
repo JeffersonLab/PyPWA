be3e8226514923652ff63fae0f0de539c2d7b995
Mark Jones <maj@jlab.org>
2017-04-10 14:07:16 -0400

    Added initial tests for option_tools.

--- tests/core/configurator/test_option_tools.py
+++ tests/core/configurator/test_option_tools.py
@@ -1,10 +1,28 @@
-import unittest
+import pytest
+
+from PyPWA.core.configurator import options
+from PyPWA.core.configurator import option_tools
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+NO_OPTIONS = option_tools.CommandOptions({})
+MIN_OPTIONS = option_tools.CommandOptions({"Option 1": 123})
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def plugin_name_conversion():
+    return option_tools.PluginNameConversion()
+
+
+def test_conversion_can_get_enum(plugin_name_conversion):
+    plugin_type = plugin_name_conversion.external_to_internal("Data Parsing")
+    assert plugin_type == options.Types.DATA_PARSER
+
+
+def test_conversion_can_get_name(plugin_name_conversion):
+    plugin_name = plugin_name_conversion.internal_to_external(
+        options.Types.KERNEL_PROCESSING
+    )
+
+    assert plugin_name == "Kernel Processor"
+
+
