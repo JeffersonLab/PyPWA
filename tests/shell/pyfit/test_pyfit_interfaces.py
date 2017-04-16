428d97f4c0f34ee3c7b568367cbcf95c717bbe4b
Mark Jones <markjonestx@gmail.com>
2017-04-16 19:18:01 -0400

    Added tests for PyFit's likelihood interfaces.

---
+++ tests/shell/pyfit/test_pyfit_interfaces.py
@@ -0,0 +1,40 @@
+import pytest
+
+from PyPWA.shell.pyfit import interfaces
+
+
+class WasCalled(Exception):
+    pass
+
+
+def processing():
+    pass
+
+
+def startup():
+    raise WasCalled
+
+
+@pytest.fixture
+def likelihood():
+    return interfaces.Likelihood(startup, processing)
+
+
+@pytest.fixture
+def setup():
+    return interfaces.Setup()
+
+
+def test_setup_function_is_passed_to_likelihood(likelihood):
+    with pytest.raises(WasCalled):
+        likelihood.setup()
+
+
+def test_process_raises_not_implemented_error(likelihood):
+    with pytest.raises(NotImplementedError):
+        likelihood.process("Some lovely data for a lovely process.")
+
+
+def test_setup_interface_raises_not_implemented_error(setup):
+    with pytest.raises(NotImplementedError):
+        setup.setup_interface()
