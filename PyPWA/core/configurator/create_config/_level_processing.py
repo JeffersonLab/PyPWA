221ef8b01dbed90ecaa6171a2ca39df35dffc152
Mark Jones <markjonestx@gmail.com>
2017-04-24 05:03:54 -0400

    Removed lingering print statement.

--- PyPWA/core/configurator/create_config/_level_processing.py
+++ PyPWA/core/configurator/create_config/_level_processing.py
@@ -115,7 +115,6 @@ class ProcessOptions(object):
 
     def __set_processed_options(self):
         self.__processed_options = self.__full_options.plugin_options
-        print(repr(self.__processed_options))
 
     def __set_difficulty_rejection_list(self, requested_difficulty):
         if requested_difficulty == options.Levels.REQUIRED:
