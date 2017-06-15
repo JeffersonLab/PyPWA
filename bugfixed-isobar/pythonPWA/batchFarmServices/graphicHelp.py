9a5daf08d382dd69755a4ff778eb2db5168ba028
Carlos Salgado <salgado@jlab.org>
2017-06-15 12:42:15 -0400

    Upgrade Isobar model: SpinDensity can be calculated in the GJ or HEL frames
    By default is using HEL now.
    There are also two gamps : gamp in the GJ and hgamp in the HEL frame.
    mode 41 (eta.pi) introduce in mass binner.
    memory set in walkNTrue

--- bugfixed-isobar/pythonPWA/batchFarmServices/graphicHelp.py
+++ bugfixed-isobar/pythonPWA/batchFarmServices/graphicHelp.py
@@ -50,40 +50,66 @@ Fill in all entry boxes.
 
 THERE ARE NO DEFAULT VALUES
 
-Reaction mode is the mode for generateAlphas:
-8 = p pi+ pi- pi0
-22 = p p+ p-
-24 = p k+ k-
-42 = gamma p --> p Ks Ks 
-(pi+ pi- pi+ pi-)eta pi n
+Reaction mode [int] (as ppgen) identify reaction for generateAlphas:
+(current supported)
+5:gamma p -> K+ Ks pi- p
+6:gamma p -> n pi+
+7:gamma p -> pi+ pi+ pi- n
+8:gamma p -> pi+ pi- pi0 p 
+9:gamma p -> pi0 p 
+15:gamma p -> e+ e- p
+21:beam p -> pi+ pi- eta p 
+22:beam p -> pi+ pi- p 
+23:beam p -> pi+ pi+ pi-  (p,n) 
+24:beam p -> K+ K- p 
+25:beam p -> (pbar p) p 
+26:beam p -> eta' pi p
+27:beam p -> pi+ pi- pi-  (p,n) 
+29:beam p -> K+ K- pi+  (p,n) 
+30:beam p -> phi eta  (p,n) 
+31:beam p -> K+ (K- p) 
+32:beam p -> pi+ (pi- p) 
+33:beam p -> pi- (pi+ p) 
+34:beam p -> K+ Kl n 
+41:beam p -> p pi0 eta 
+42:beam p -> Kshort (-> pi+ pi-) KL p
+43:beam p -> phi omega p
 
-Beam polarization is an important value for some 
-experiments. It must be a float value!
-No polarization = 0.0
+Beam polarization [float]: Fraction of Linear Polearization
+No polarization = 0.0, full = 1.0
 
-The Upper, lower, and range of the mass are inclusive 
-and will affect binning.
+Angle of polarization [float] (in degrees): Angle of vector Polarization from
+the x axis (Parallel to lab floor) PARA=0,PERP=90 
 
-Sets is almost never used.
+The Upper [int], lower [int], and range [int] of a mass bin (in MeV).
+The number of mass bins will be then: (Upper-lower)/range
+
+Frame Definition [sting]: The frame where to generate the resonance:
+ GJ (Godfried-Jackson), HEL (Helicity), AD (Adair)
+
+Sets is almost never used [int] (in case the MC has been divided in sets).
 Put 0
 
-The number of Migrad calls should be at least 1000, 
+In Minuit: The max number of Migrad calls should be at least 1000 [int], 
 but any more will depend on other factors, like 
 number of events and waves.
 
 Name of tested reaction should just be a quick title,
 possibly the same as the top directory of the project. 
 It will be used as the title of graphs made by 
-graphicPlot.
+graphicPlot. [string]
 
-Name of the saved data is the filename where graphicPlot
-will save the CURRENT data loaded to it and where it will
-look to load already saved data, they are all saved inside 
+Name for saved data (mostly same as before) It is the filename 
+where graphicPlot will save the CURRENT data loaded to it and where 
+it will look to load already saved data, they are all saved inside 
 /GUI/plotLists. Change this as often as you need to make 
 as many different sets of data and to view different sets 
 of data by using the pwa_controls button on the graphicPlot 
 button panel. But if you update anything and save over an
-old file, then the old data will be lost.
+old file, then the old data will be lost. [string]
+
+Project: CLAS or GlueX [string]
+
 
 Press save when done.
 
