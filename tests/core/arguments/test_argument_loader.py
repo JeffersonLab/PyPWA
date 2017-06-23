5c3f1076aca53c00f620c788e0283a920761c249
Mark Jones <maj@jlab.org>
2017-06-23 13:11:30 -0400

    Update tests for arguments
    Tests needed to be updated since the loader now returns initialized
    objects and since the plugins metadata for arguments has changed.

--- tests/core/arguments/test_argument_loader.py
+++ tests/core/arguments/test_argument_loader.py
@@ -2,6 +2,8 @@ import os
 
 import pytest
 
+from PyPWA.builtin_plugins import data
+from PyPWA.shell import blank
 from PyPWA.core.arguments import _loader
 from PyPWA.core.shared.interfaces import plugins
 
@@ -19,15 +21,15 @@ def loader():
 def test_loader_can_find_data_parser(loader):
     # type: (_loader.RequestedFetcher) -> None
     plugin = loader.get_plugin_by_name("Builtin Parser")
-    assert issubclass(plugin, plugins.BasePlugin)
-    assert plugin.plugin_name == "Builtin Parser"
+    assert isinstance(plugin, data.ArgDataParse)
+    assert plugin.get_name() == "Builtin Parser"
 
 
 def test_loader_can_find_blank_module(loader):
     # type: (_loader.RequestedFetcher) -> None
     main = loader.get_plugin_by_name("blank shell module")
-    assert issubclass(main, plugins.BasePlugin)
-    assert main.plugin_name == "blank shell module"
+    assert isinstance(main, blank.BlankArguments)
+    assert main.get_name() == "blank shell module"
 
 
 def test_loader_raises_error_with_unknown(loader):
