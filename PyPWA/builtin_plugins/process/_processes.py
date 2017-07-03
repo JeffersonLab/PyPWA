f043a1b2f917ed3e0e0ab43b76832295a0b375a3
Mark Jones <markjonestx@live.com>
2017-07-03 13:53:01 -0400

    Improves Processes
    Adds an abstract Process to setup daemon mode for all processes. Cleans
    the code to adhere to our coding standards, and adds error handling for
    cases when the process's kernel might crash so that the parent thread
    can be notified correctly.

--- PyPWA/builtin_plugins/process/_processes.py
+++ PyPWA/builtin_plugins/process/_processes.py
@@ -15,77 +15,100 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-The processes and their factories are defined here. The current supported
-methods are Duplex for worker processes and Simplex for offload processes.
+
 """
-from PyPWA.core.shared.interfaces import internals
-from PyPWA.builtin_plugins.process.communication import _interface
+
+import enum
+import logging
 import multiprocessing
+from multiprocessing import connection
 
-from PyPWA import VERSION, LICENSE, STATUS
+from PyPWA import VERSION, AUTHOR
+from PyPWA.core.shared.interfaces import internals
 
-__author__ = ["Mark Jones"]
 __credits__ = ["Mark Jones"]
-__maintainer__ = ["Mark Jones"]
-__email__ = "maj@jlab.org"
-__status__ = STATUS
-__license__ = LICENSE
+__author__ = AUTHOR
 __version__ = VERSION
 
 
-class Duplex(multiprocessing.Process):
+class ProcessCodes(enum.Enum):
 
-    daemon = True  # When true, processes will die with main
+    SHUTDOWN = 1
+    ERROR = 2
 
-    __kernel = None
-    __communicator = None
 
-    __should_calculate = True
-    __received_value = None
+class _AbstractProcess(multiprocessing.Process):
 
-    def __init__(self, kernel, communicator):
+    def __init__(self, kernel, connect):
+        # type: (internals.Kernel, connection.Connection) -> None
+        super(_AbstractProcess, self).__init__()
+        self.daemon = True  # When true, processes will die with main
+
+    def run(self):
+        raise NotImplementedError
+
+
+class Duplex(_AbstractProcess):
+
+    __LOGGER = logging.getLogger(__name__ + ".Duplex")
+
+    def __init__(self, kernel, connect):
+        # type: (internals.Kernel, connection.Connection) -> None
         super(Duplex, self).__init__()
         self.__kernel = kernel
-        self.__communicator = communicator
+        self.__connection = connect
+        self.__received_value = None
 
     def run(self):
         self.__kernel.setup()
         self.__loop()
-        return 0
 
     def __loop(self):
-        while self.__should_calculate:
+        while True:
             self.__get_value()
-            if self.__received_value == "DIE":
-                self.__should_calculate = False
-            else:
-                self.__process()
+            if self.__received_value == ProcessCodes.SHUTDOWN:
+                self.__LOGGER.debug("Gracefully shutting down process.")
+                break
+            self.__process()
 
     def __get_value(self):
-        value = self.__communicator.receive()
-        self.__received_value = value
+        self.__received_value = self.__connection.recv()
 
     def __process(self):
-        processed_data = self.__kernel.process(self.__received_value)
-        self.__communicator.send(processed_data)
+        try:
+            value = self.__run_kernel()
+        except Exception as error:
+            self.__handle_error(error)
+        else:
+            self.__connection.send(value)
+
+    def __run_kernel(self):
+        return self.__kernel.process(self.__received_value)
+
+    def __handle_error(self, error):
+        self.__connection.send(ProcessCodes.ERROR)
+        self.__LOGGER.exception(error)
+        self.__LOGGER.critical(
+            "Child process in critical state! The program will crash!"
+        )
+        raise error
 
 
-class Simplex(multiprocessing.Process):
+class Simplex(_AbstractProcess):
 
-    daemon = True  # When true, processes will die with main
-    __kernel = None  # type: internals.Kernel
-    __communicator = None  # type: _interface._Communication
-
-    def __init__(self, single_kernel, communicator):
+    def __init__(self, single_kernel, connect):
+        # type: (internals.Kernel, connection.Connection) -> None
         super(Simplex, self).__init__()
         self.__kernel = single_kernel
-        self.__communicator = communicator
+        self.__connection = connect
 
     def run(self):
         self.__kernel.setup()
-        self.__communicator.send(self.__kernel.process())
-        return 0
+        self.__process()
 
-    def _process(self):
-        processed_data = self.__kernel.process()
-        self.__communicator.send(processed_data)
+    def __process(self):
+        try:
+            self.__connection.send(self.__kernel.process())
+        except Exception as error:
+            self.__connection.send(ProcessCodes.ERROR)
+            raise error
