Fixes #124 incorrect value order and broken math
This adds tests for the Vectors to catch and value sorting errors.
Originally the order of the variables was x, y, z, e, and eventually
that order was changed to e, x, y, z, since that is the standard
ordering of values. There was errors produced from that change, and
this should patch those errors.

Next, the sanitization function was well tested with arrays, but was
loosely tested for single values since in our use-cases we did not use
any other than inputs. This should patch those issues out as well,
and there is now more testing to catch potential errors that might
arrise in the future from that.

Lastly, I added iPython and Jupyter pretty output while I was here,
so with using iPython or Jupyter there should be some nice output
to explain what the Vector stores. If you want to see the raw data
inside the container, you can still use repr(vector).

PyPWA/libs/vectors/_base_vector.py
@@ -43,7 +43,12 @@ def sanitize_vector_input(a, b=None, c=None, d=None, has_e=False):
         names.insert(0, "e")
 
     # Produce empty arrays of length X
-    if isinstance(a, int):
+    if isinstance(a, int) and isinstance(b, type(None)):
+        if a == 0:
+            if has_e:
+                return np.float(0), np.float(0), np.float(0), np.float(0)
+            return np.float(0), np.float(0), np.float(0)
+
         d = [np.zeros(a), np.zeros(a), np.zeros(a)]
         if has_e:
             d.append(np.zeros(a))
@@ -56,25 +61,25 @@ def sanitize_vector_input(a, b=None, c=None, d=None, has_e=False):
 
     # Pass through single values
     elif all([isinstance(var, (int, float)) for var in [a, b, c]]):
-        returns = [a, b, c]
+        returns = [np.float(a), np.float(b), np.float(c)]
         if has_e:
             if not isinstance(d, (int, float)):
-                raise ValueError("No E value provided!")
+                raise ValueError("No Z value provided!")
             else:
-                returns.insert(0, d)
+                returns.append(np.float(d))
         return returns
 
     # Convert Structured Arrays to Contiguous Arrays
     elif all([isinstance(var, np.ndarray) for var in [a, b, c]]):
         if has_e:
             if not isinstance(d, np.ndarray):
-                raise ValueError("No E Value provided!")
+                raise ValueError("No Z Value provided!")
             else:
                 if all([d.flags["C_CONTIGUOUS"]] for d in [a, b, c, d]):
                     return a, b, c, d
                 else:
                     return common.to_contiguous(
-                        {"x": a, "y": b, "z": c, "e": d}, names
+                        {"e": a, "x": b, "y": c, "z": d}, names
                     )
         else:
             if all([d.flags["C_CONTIGUOUS"]] for d in [a, b, c]):

PyPWA/libs/vectors/four_vector.py
@@ -66,22 +66,38 @@ class FourVector(_base_vector.VectorMath):
             self._y = e._y
             self._z = e._z
         else:
-            self._x, self._y, self._z, self._e = \
+            self._e, self._x, self._y, self._z = \
                 _base_vector.sanitize_vector_input(e, x, y, z, True)
         super(FourVector, self).__init__(self._x, self._y, self._z)
 
-    def __repr__(self) -> str:
-        theta, phi, mass = self._get_repr_data()
+    def __repr__(self):
+        return f"FourVector(e={self.e}, x={self.x}, y={self.y}, z={self.z})"
 
-        return f"FourVector(x̅Θ={theta}, x̅ϕ={phi}, x̅Mass={mass})"
+    def _repr_pretty_(self, p, cycle):
+        if cycle:
+            p.text("FourVector( ?.)")
+
+        else:
+            theta, phi, mass = self._get_repr_data()
+            p.text(f"FourVector(x̅Θ={theta}, x̅ϕ={phi}, x̅Mass={mass})")
 
     def _get_repr_data(self):
+        # Theta needs to be set to NaN if we're working with
+        # uninitialized values
         if isinstance(self._e, np.ndarray):
-            theta = self.get_theta().mean()
+            if all(self._z) == 0.0:
+                theta = np.NaN
+            else:
+                theta = self.get_theta().mean()
+
             phi = self.get_phi().mean()
             mass = self.get_mass().mean()
         else:
-            theta = self.get_theta()
+            if self._z == 0.0:
+                theta = np.NaN
+            else:
+                theta = self.get_theta()
+
             phi = self.get_phi()
             mass = self.get_mass()
         return theta, phi, mass
@@ -159,6 +175,8 @@ class FourVector(_base_vector.VectorMath):
         return self.__sub__(other)
 
     def __len__(self):
+        if isinstance(self._x, float):
+            return 0
         return len(self._x)
 
     def __getitem__(

PyPWA/libs/vectors/three_vector.py
@@ -65,14 +65,28 @@ class ThreeVector(_base_vector.VectorMath):
         super(ThreeVector, self).__init__(self._x, self._y, self._z)
 
     def __repr__(self) -> str:
-        if isinstance(self._x, np.ndarray):
-            theta = self.get_theta().mean()
-            phi = self.get_phi().mean()
-        else:
-            theta = self.get_theta()
-            phi = self.get_phi()
+        return f"ThreeVector(x={self._x}, y={self._y}, z={self._z})"
 
-        return f"ThreeVector(x̅Θ={theta}, x̅ϕ={phi})"
+    def _repr_pretty_(self, p, cycle):
+        if cycle:
+            p.text("ThreeVector( ?.)")
+        else:
+            if isinstance(self._x, np.ndarray):
+                if all(self._z) == 0.0:
+                    theta = np.NaN
+                else:
+                    theta = self.get_theta().mean()
+
+                phi = self.get_phi().mean()
+            else:
+                if self._z == 0:
+                    theta = np.NaN
+                else:
+                    theta = self.get_theta()
+
+                phi = self.get_phi()
+
+            p.text(f"ThreeVector(x̅Θ={theta}, x̅ϕ={phi})")
 
     def __eq__(self, vector: "ThreeVector") -> bool:
         if isinstance(vector, ThreeVector):